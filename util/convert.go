3b8b23cfe265fb9959d588fca7bfafb79f760f60
Michael Whatcott <mdwhatcott@gmail.com>
2017-12-01 22:42:29 -0700

    Parse many numeric strings in a single operation.
    Would have come in handing for day 02 of 2017./

--- util/convert.go
+++ util/convert.go
@@ -6,3 +6,10 @@ func ParseInt(value string) int {
 	parsed, _ := strconv.Atoi(value)
 	return parsed
 }
+
+func ParseInts(values []string) (ints []int) {
+	for _, value := range values {
+		ints = append(ints, ParseInt(value))
+	}
+	return ints
+}
