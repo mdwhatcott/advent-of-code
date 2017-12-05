e2553807a7c62819765a50811f78ddb09d8d5046
Michael Whatcott <mdwhatcott@gmail.com>
2017-12-04 22:51:28 -0700

    Minor tweaks to the grid. Also providing vars for directions.

--- grid/point.go
+++ grid/point.go
@@ -23,19 +23,41 @@ func (this Point) String() string {
 }
 
 func (this Point) Neighbors4() (neighbors []Point) {
-	neighbors = append(neighbors, NewPoint(this.x+1, this.y+0)) // right
-	neighbors = append(neighbors, NewPoint(this.x-1, this.y+0)) // left
-	neighbors = append(neighbors, NewPoint(this.x+0, this.y+1)) // top
-	neighbors = append(neighbors, NewPoint(this.x+0, this.y-1)) // bottom
-
+	for _, offset := range Neighbors4 {
+		neighbors = append(neighbors, this.Offset(offset.dx, offset.dy))
+	}
 	return neighbors
 }
 
 func (this Point) Neighbors8() (neighbors []Point) {
-	neighbors = this.Neighbors4()
-	neighbors = append(neighbors, NewPoint(this.x+1, this.y+1)) // top-right
-	neighbors = append(neighbors, NewPoint(this.x-1, this.y-1)) // bottom-left
-	neighbors = append(neighbors, NewPoint(this.x+1, this.y-1)) // bottom-right
-	neighbors = append(neighbors, NewPoint(this.x-1, this.y+1)) // top-right
+	for _, offset := range Neighbors8 {
+		neighbors = append(neighbors, this.Offset(offset.dx, offset.dy))
+	}
 	return neighbors
 }
+
+//////////////////////////////////////////////////////////
+
+type Direction struct{ dx, dy float64 }
+
+func NewDirection(dx, dy float64) Direction {
+	return Direction{dx: dx, dy: dy}
+}
+
+func (this Direction) Dx() float64 { return this.dx }
+func (this Direction) Dy() float64 { return this.dy }
+
+var (
+	Right = Direction{dx: 1, dy: 0}
+	Left  = Direction{dx: -1, dy: 0}
+	Up    = Direction{dx: 0, dy: 1}
+	Down  = Direction{dx: 0, dy: -1}
+
+	TopRight    = Direction{1, 1}
+	TopLeft     = Direction{-1, 1}
+	BottomRight = Direction{1, -1}
+	BottomLeft  = Direction{-1, -1}
+
+	Neighbors4 = []Direction{Right, Left, Up, Down}
+	Neighbors8 = append(Neighbors4, []Direction{TopRight, TopLeft, BottomRight, BottomLeft}...)
+)
