6b66c46048ca06e068ceef88a671b5770f454f56
Michael Whatcott <mdwhatcott@gmail.com>
2018-01-11 21:19:53 -0700

    Implemented a few binary functions.

--- util/convert.go
+++ util/convert.go
@@ -1,6 +1,9 @@
 package util
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func ParseInt(value string) int {
 	parsed, _ := strconv.Atoi(value)
@@ -12,4 +15,20 @@ func ParseInts(values []string) (ints []int) {
 		ints = append(ints, ParseInt(value))
 	}
 	return ints
+}
+
+// See: https://en.wikipedia.org/wiki/Hamming_weight
+func BinaryHammingWeight(value int) (count int) {
+	for count = 0; value > 0; count++ {
+		value &= value - 1
+	}
+	return count
+}
+
+func EncodeBinary(value byte) string {
+	var buffer = make([]string, 8)
+	for i := 0; i < 8; i++ {
+		buffer[7-i] = strconv.Itoa((int(value) >> uint(i)) & 1)
+	}
+	return strings.Join(buffer, "")
 }
