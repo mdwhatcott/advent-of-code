868389a9e4452029b1523d2b2f4a7bc714f42cec
Michael Whatcott <mdwhatcott@gmail.com>
2018-10-03 20:05:04 -0600

    Alternate implementation with pre-allocated slice.

--- 2017/day17/main.go
+++ 2017/day17/main.go
@@ -2,15 +2,18 @@ package day17
 
 import (
 	"container/ring"
+	"fmt"
+	"time"
 
 	"advent/lib/util"
 )
 
 func Part1() interface{} {
+	steps := util.InputInt()
 	circle := ring.New(1)
 	circle.Value = 0
 	for x := 1; x < 2018; x++ {
-		for y := 0; y < util.InputInt(); y++ {
+		for y := 0; y < steps; y++ {
 			circle = circle.Next()
 		}
 		insert := ring.New(1)
@@ -21,6 +24,29 @@ func Part1() interface{} {
 	return circle.Next().Value
 }
 
-func Part2() int {
-	return 0
+func Part2() interface{} {
+	now := time.Now()
+	length := 50000000
+	circle := make([]int, 1, length)
+	steps := util.InputInt()
+	current := 0
+	for x := 1; x < length+1; x++ {
+		if x%100000 == 0 {
+			fmt.Println(x, time.Since(now))
+			now = time.Now()
+		}
+		insert := advance(current, len(circle), steps) + 1
+		current = insert
+
+		circle = append(circle, 0)
+		copy(circle[insert+1:], circle[insert:])
+		circle[insert] = x
+
+	}
+	fmt.Println(circle)
+	return circle[current+1]
+}
+
+func advance(current, length, steps int) int {
+	return (current + steps) % length
 }
