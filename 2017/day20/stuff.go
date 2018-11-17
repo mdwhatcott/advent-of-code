505406dbbc62187d14808c2b638b4c266cad6472
Michael Whatcott <mdwhatcott@gmail.com>
2018-11-16 21:52:28 -0700

    Solved 2017 day 20.

--- 2017/day20/stuff.go
+++ 2017/day20/stuff.go
@@ -1,1 +1,75 @@
 package day20
+
+import (
+	"strings"
+
+	"advent/lib/util"
+)
+
+func ParsePixel(id int, line string) *Pixel {
+	pixel := &Pixel{ID: id}
+	pixel.Point = ParseFirstTriplet(line)
+	line = line[strings.Index(line, ">")+1:]
+	pixel.Velocity = ParseFirstTriplet(line)
+	line = line[strings.Index(line, ">")+1:]
+	pixel.Acceleration = ParseFirstTriplet(line)
+	return pixel
+}
+
+func ParseFirstTriplet(line string) Triplet {
+	start := strings.Index(line, "<")
+	stop := strings.Index(line, ">")
+	excerpt := line[start+1 : stop]
+	fields := strings.Split(excerpt, ",")
+	return Triplet{
+		X: util.ParseInt(fields[0]),
+		Y: util.ParseInt(fields[1]),
+		Z: util.ParseInt(fields[2]),
+	}
+}
+
+type Pixel struct {
+	ID           int
+	Point        Triplet
+	Velocity     Triplet
+	Acceleration Triplet
+}
+
+type Triplet struct{ X, Y, Z int }
+
+func (this Triplet) Apply(that Triplet) Triplet {
+	return Triplet{
+		X: this.X + that.X,
+		Y: this.Y + that.Y,
+		Z: this.Z + that.Z,
+	}
+}
+
+func (this *Pixel) Update() {
+	this.Velocity = this.Velocity.Apply(this.Acceleration)
+	this.Point = this.Point.Apply(this.Velocity)
+}
+
+func (this *Pixel) DistanceFromOrigin() int {
+	return abs(this.Point.X) + abs(this.Point.Y) + abs(this.Point.Z)
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
+func detectCollisions(pixels []*Pixel) map[Triplet][]int {
+	collisions := make(map[Triplet][]int)
+	for _, pixel := range pixels {
+		collisions[pixel.Point] = append(collisions[pixel.Point], pixel.ID)
+	}
+	for point := range collisions {
+		if len(collisions[point]) == 1 {
+			delete(collisions, point)
+		}
+	}
+	return collisions
+}
