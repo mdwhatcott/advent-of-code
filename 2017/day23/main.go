0b5825fc4253c11d6f8aea37dd73c4c86188d89e
Michael Whatcott <mdwhatcott@gmail.com>
2018-11-26 23:39:31 -0700

    Implemented 2017 day 23. Assembly is a beast.

--- 2017/day23/main.go
+++ 2017/day23/main.go
@@ -1,9 +1,35 @@
 package day23
 
+import (
+	"math"
+
+	"advent/lib/util"
+)
+
 func Part1() int {
-	return 0
+	interpreter := NewInterpreter(util.InputLines())
+	interpreter.Run()
+	return interpreter.mul
 }
 
-func Part2() int {
-	return 0
+// Total rip-off: https://www.reddit.com/r/adventofcode/comments/7lms6p/2017_day_23_solutions/drnmlbk/
+func Part2() (h int) {
+	c := 125100 // a const given my input
+	b := 108100 // a const given my input
+
+	for ; b < c + 1; b += 17 {
+		if !prime(b) {
+			h += 1
+		}
+	}
+	return h
+}
+
+func prime(i int) bool {
+	for j := 2; j < int(math.Sqrt(float64(i))) + 1; j++ {
+		if i % j == 0 {
+			return false
+		}
+	}
+	return true
 }
