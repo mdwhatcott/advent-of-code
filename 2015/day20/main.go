47f26e04b221e4ea25df4b4c50e1aefc60521ad2
Michael Whatcott <mdwhatcott@gmail.com>
2018-11-30 23:50:27 -0700

    Solved 2015 day 20 part 1

--- 2015/day20/main.go
+++ 2015/day20/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+
+	"advent/lib/util"
+)
+
 func main() {
+	max := 0
+	startingHouse := 660000 // found by trial and error
+	for house := startingHouse; ; house++ {
+		count := presents(house)
+		if count > max {
+			fmt.Println("Progress:", house, count)
+			max = count
+		}
+		if count >= util.InputInt() {
+			fmt.Println("Part 1:", house, count) // 665280
+			break
+		}
+	}
+}
+
+func presents(house int) (presents int) {
+	for elf := 1; elf <= house; elf++ {
+		if house%elf == 0 {
+			presents += elf * 10
+		}
+	}
+	return presents
 }
