1720c80a7f7294b5356e73b8dc57e8ed87f94135
Michael Whatcott <mdwhatcott@gmail.com>
2018-12-01 15:05:00 -0700

    Solved 2015 day 20 part 2.

--- 2015/day20/main.go
+++ 2015/day20/main.go
@@ -7,6 +7,12 @@ import (
 )
 
 func main() {
+	fmt.Println("Part 1 Answer:", part1()) // 665280
+	fmt.Println("Part 2 Answer:", part2()) // 705600
+}
+
+func part1() int {
+	input := util.InputInt()
 	max := 0
 	startingHouse := 660000 // found by trial and error
 	for house := startingHouse; ; house++ {
@@ -15,9 +21,8 @@ func main() {
 			fmt.Println("Progress:", house, count)
 			max = count
 		}
-		if count >= util.InputInt() {
-			fmt.Println("Part 1:", house, count) // 665280
-			break
+		if count >= input {
+			return house
 		}
 	}
 }
@@ -29,4 +34,53 @@ func presents(house int) (presents int) {
 		}
 	}
 	return presents
+}
+
+func part2() int {
+	input := util.InputInt()
+	max := 0
+	presents := NewPresents()
+	startingHouse := 700000
+	for house := startingHouse; ; house++ {
+		count := presents.Deliver(house)
+		if count > max {
+			fmt.Println("Progress:", house, count)
+			max = count
+		}
+		if count >= input {
+			return house
+		}
+	}
+}
+
+type Presents struct {
+	deliveries map[int]int
+	minElf     int
+}
+
+func NewPresents() *Presents {
+	return &Presents{deliveries: make(map[int]int), minElf: 1}
+}
+
+func (this *Presents) Deliver(house int) (presents int) {
+	for elf := this.minElf; elf <= house; elf++ {
+		delivered := this.deliveries[elf]
+		if house%elf == 0 && delivered < 10 {
+			presents += elf * 10
+			this.deliveries[elf]++
+		}
+	}
+	this.incrementMinElf()
+	return presents
+}
+
+func (this *Presents) incrementMinElf() {
+	for {
+		delivered := this.deliveries[this.minElf]
+		if delivered == 10 {
+			this.minElf++
+		} else {
+			break
+		}
+	}
 }
