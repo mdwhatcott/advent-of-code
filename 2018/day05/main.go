7e3ba3f452096975a12f3150289dec45a52f1358
Michael Whatcott <mdwhatcott@gmail.com>
2018-12-07 23:09:46 -0700

    Solved 2018 day 5.

--- 2018/day05/main.go
+++ 2018/day05/main.go
@@ -1,9 +1,11 @@
 package day05
 
+import "advent/lib/util"
+
 func Part1() interface{} {
-	return nil
+	return len(react(util.InputString()))
 }
 
 func Part2() interface{} {
-	return nil
+	return len(reactAggressive(util.InputString()))
 }
--- 2018/day05/stuff.go
+++ 2018/day05/stuff.go
@@ -1,4 +1,40 @@
 package day05
 
-type Stuff struct {
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
+
+func react(s string) string {
+	b := bytes.NewBufferString(s[:1])
+	for x := 1; x < len(s); x++ {
+		if b.Len() == 0 {
+			b.WriteByte(s[x])
+			x++
+		}
+		this, last := s[x], b.Bytes()[b.Len()-1]
+		if this != last && strings.ToUpper(string(this)) == strings.ToUpper(string(last)) {
+			b = bytes.NewBuffer(b.Bytes()[:b.Len()-1])
+		} else {
+			b.WriteByte(this)
+		}
+	}
+	return b.String()
+}
+func reactAggressive(s string) string {
+	minLength := len(s)
+	minPolymer := s
+	for _, c := range "abcdefghijklmnopqrstuvwxyz" {
+		ss := s
+		ss = strings.Replace(ss, string(c), "", -1)
+		ss = strings.Replace(ss, string(unicode.ToUpper(c)), "", -1)
+		polymer := react(ss)
+		length := len(polymer)
+		if length < minLength {
+			minLength = length
+			minPolymer = polymer
+		}
+	}
+	return minPolymer
 }
