3c539e49c25ee06801ec83f8868c09c6d2b9b7b4
Michael Whatcott <mdwhatcott@gmail.com>
2018-12-25 12:32:06 -0700

    Reset.

--- 2018/day07/main.go
+++ 2018/day07/main.go
@@ -1,15 +1,9 @@
 package day07
 
-import (
-	"advent/lib/util"
-)
-
 func Part1() interface{} {
-	return NewTopologicalSort(util.InputString()).Sort()
+	return nil
 }
 
 func Part2() interface{} {
-	sorter := NewConcurrentTopologicalSort(util.InputString(), 5, 60)
-	sorter.Sort()
-	return sorter.DurationSeconds()
+	return nil
 }
