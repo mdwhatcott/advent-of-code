05315f302899016248366a8c061d799789f41975
Michael Whatcott <mdwhatcott@gmail.com>
2018-12-31 22:11:49 -0700

    Solved 2018 Day 09

--- 2018/day09/stuff.go
+++ 2018/day09/stuff.go
@@ -1,4 +1,56 @@
 package day09
 
-type Stuff struct {
+import (
+	"container/ring"
+	"fmt"
+	"strings"
+
+	"advent/lib/util"
+)
+
+func Parse(input string) (players int, maxMarble int) {
+	fields := strings.Fields(input)
+	players = util.ParseInt(fields[0])
+	maxMarble = util.ParseInt(fields[6])
+	return players, maxMarble
+}
+
+func Parse10X(input string) (players int, maxMarble int) {
+	players, maxMarble = Parse(input)
+	return players, maxMarble * 100
+}
+
+func MarbleHighScore(playerCount int, maxMarble int) int {
+	var players = make([]int, playerCount)
+	start := ring.New(1)
+	start.Value = 0
+	circle := start
+	for marble := 1; marble <= maxMarble; marble++ {
+		if marble%23 == 0 {
+			players[marble%playerCount] += marble
+			for x := 0; x < 9; x++ {
+				circle = circle.Prev()
+			}
+			removed := circle.Unlink(1)
+			players[marble%playerCount] += removed.Value.(int)
+			circle = circle.Next().Next()
+		} else {
+			insert := ring.New(1)
+			insert.Value = marble
+			circle = circle.Link(insert)
+		}
+		//debug(start, marble)
+	}
+	return util.Max(players...)
+}
+
+func debug(circle *ring.Ring, marble int) {
+	circle.Do(func(f interface{}) {
+		if f == marble {
+			fmt.Printf("(%v) ", f)
+		} else {
+			fmt.Print(f, " ")
+		}
+	})
+	fmt.Println()
 }
--- 2018/day09/stuff_test.go
+++ 2018/day09/stuff_test.go
@@ -3,6 +3,7 @@ package day09
 import (
 	"testing"
 
+	"github.com/smartystreets/assertions/should"
 	"github.com/smartystreets/gunit"
 )
 
@@ -14,8 +15,12 @@ type StuffFixture struct {
     *gunit.Fixture
 }
 
-func (this *StuffFixture) Setup() {
+func (this *StuffFixture) TestExamples() {
+	this.So(MarbleHighScore(Parse("9 players; last marble is worth 25 points")), should.Equal, 32)
+	this.So(MarbleHighScore(Parse("10 players; last marble is worth 1618 points")), should.Equal, 8317)
+	this.So(MarbleHighScore(Parse("13 players; last marble is worth 7999 points")), should.Equal, 146373)
+	this.So(MarbleHighScore(Parse("17 players; last marble is worth 1104 points")), should.Equal, 2764)
+	this.So(MarbleHighScore(Parse("21 players; last marble is worth 6111 points")), should.Equal, 54718)
+	this.So(MarbleHighScore(Parse("30 players; last marble is worth 5807 points")), should.Equal, 37305)
 }
 
-func (this *StuffFixture) Test() {
-}
