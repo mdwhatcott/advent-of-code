a09c24fa35f0dc98f82e4de4fe135f46bc827c2c
Michael Whatcott <mdwhatcott@gmail.com>
2018-12-31 22:11:29 -0700

    Filled in main for day 7

--- 2018/day07/main.go
+++ 2018/day07/main.go
@@ -1,9 +1,13 @@
 package day07
 
+import "advent/lib/util"
+
 func Part1() interface{} {
-	return nil
+	order1, _ := NewTopologicalSort(util.InputString(), 1, 1).Sort()
+	return order1
 }
 
 func Part2() interface{} {
-	return nil
+	_, seconds5 := NewTopologicalSort(util.InputString(), 5, 60).Sort()
+	return seconds5
 }
