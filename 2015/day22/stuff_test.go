5b1e2215e803989e252c2520714fb6126f8bf307
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-01 22:23:20 -0600

    Several more tests.

--- 2015/day22/stuff_test.go
+++ 2015/day22/stuff_test.go
@@ -8,19 +8,49 @@ import (
 )
 
 func TestStuff(t *testing.T) {
-	gunit.Run(new(Stuff), t)
+	gunit.Run(new(BattleFixture), t)
 }
 
-type Stuff struct {
+type BattleFixture struct {
 	*gunit.Fixture
 }
 
-func (this *Stuff) Test_PlayerTurn_BothStillAlive_NoEffectsInPlay_PlentyOfMana__AllSpellsAvailable() {
+func (this *BattleFixture) Test_PlayerTurn_ButIsDead_NoMoves() {
 	game := Battle{
 		IsPlayerTurn:    true,
-		PlayerHitPoints: 10,
+		PlayerHitPoints: 0,
 		PlayerMana:      250,
-		BossHitPoints:   13,
+		BossHitPoints:   1,
+	}
+	this.So(game.NextMoves(), should.BeEmpty)
+}
+
+func (this *BattleFixture) Test_PlayerTurn_ButManaIsScarce_NoMoves() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      52,
+		BossHitPoints:   1,
+	}
+	this.So(game.NextMoves(), should.BeEmpty)
+}
+
+func (this *BattleFixture) Test_BossTurn_ButIsDead_NoMoves() {
+	game := Battle{
+		IsPlayerTurn:    false,
+		PlayerHitPoints: 1,
+		PlayerMana:      250,
+		BossHitPoints:   0,
+	}
+	this.So(game.NextMoves(), should.BeEmpty)
+}
+
+func (this *BattleFixture) Test_PlayerTurn_BothStillAlive_NoEffectsInPlay_PlentyOfMana__AllSpellsAvailable() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      250,
+		BossHitPoints:   1,
 	}
 
 	this.So(game.NextMoves(), should.Resemble, []Turn{
@@ -32,13 +62,108 @@ func (this *Stuff) Test_PlayerTurn_BothStillAlive_NoEffectsInPlay_PlentyOfMana__
 	})
 }
 
-func (this *Stuff) Test_BossTurn_BothStillAlive_Attack() {
+func (this *BattleFixture) Test_BossTurn_BothStillAlive_Attack() {
 	game := Battle{
 		IsPlayerTurn:    false,
-		PlayerHitPoints: 10,
+		PlayerHitPoints: 1,
 		PlayerMana:      250,
-		BossHitPoints:   13,
+		BossHitPoints:   1,
 	}
 
 	this.So(game.NextMoves(), should.Resemble, []Turn{new(BossAttack)})
+}
+
+func (this *BattleFixture) Test_PlayerTurn_NotEnoughManaForRecharge() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      228,
+		BossHitPoints:   1,
+	}
+
+	this.So(game.NextMoves(), should.Resemble, []Turn{
+		new(Missile),
+		new(Drain),
+		new(Shield),
+		new(Poison),
+		//new(Recharge),
+	})
+}
+
+func (this *BattleFixture) Test_PlayerTurn_NotEnoughManaForPoison() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      172,
+		BossHitPoints:   1,
+	}
+
+	this.So(game.NextMoves(), should.Resemble, []Turn{
+		new(Missile),
+		new(Drain),
+		new(Shield),
+		//new(Poison),
+		//new(Recharge),
+	})
+}
+
+func (this *BattleFixture) Test_PlayerTurn_NotEnoughManaForShield() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      112,
+		BossHitPoints:   1,
+	}
+
+	this.So(game.NextMoves(), should.Resemble, []Turn{
+		new(Missile),
+		new(Drain),
+		//new(Shield),
+		//new(Poison),
+		//new(Recharge),
+	})
+}
+
+func (this *BattleFixture) Test_PlayerTurn_NotEnoughManaForDraim() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 1,
+		PlayerMana:      72,
+		BossHitPoints:   1,
+	}
+
+	this.So(game.NextMoves(), should.Resemble, []Turn{
+		new(Missile),
+		//new(Drain),
+		//new(Shield),
+		//new(Poison),
+		//new(Recharge),
+	})
+}
+
+func (this *BattleFixture) SkipTestExample1() {
+	game := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 10,
+		PlayerMana:      250,
+		BossHitPoints:   13,
+		BossDamage:      8,
+	}
+
+	moves := []Turn{
+		new(Poison),
+		new(BossAttack),
+		new(Missile),
+	}
+	for _, turn := range moves {
+		game = turn.Perform(game)
+	}
+
+	this.So(game, should.Resemble, Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 2,
+		PlayerMana:      77,
+		PoisonCounter:   4,
+		BossDamage:      8,
+	})
 }
