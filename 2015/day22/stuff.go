4c4ef680b3eaf85483a26a3e758616618557a7b8
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-01 22:23:20 -0600

    Several more tests.

--- 2015/day22/stuff.go
+++ 2015/day22/stuff.go
@@ -16,14 +16,36 @@ type Battle struct {
 }
 
 func (this Battle) NextMoves() (moves []Turn) {
-	if this.IsPlayerTurn {
-		moves = append(moves, new(Missile))
-		moves = append(moves, new(Drain))
-		moves = append(moves, new(Shield))
-		moves = append(moves, new(Poison))
-		moves = append(moves, new(Recharge))
+	if this.gameOver() {
+		return nil
+	} else if this.IsPlayerTurn {
+		return this.collectPlayerMoves()
 	} else {
-		moves = append(moves, new(BossAttack))
+		return append(moves, new(BossAttack))
+	}
+}
+
+func (this Battle) collectPlayerMoves() (moves []Turn) {
+	if this.PlayerMana >= 53 {
+		moves = append(moves, new(Missile))
+	}
+	if this.PlayerMana >= 73 {
+		moves = append(moves, new(Drain))
+	}
+	if this.PlayerMana >= 113 {
+		moves = append(moves, new(Shield))
+	}
+	if this.PlayerMana >= 173 {
+		moves = append(moves, new(Poison))
+	}
+	if this.PlayerMana >= 229 {
+		moves = append(moves, new(Recharge))
 	}
 	return moves
 }
+
+func (this Battle) gameOver() bool {
+	return (this.IsPlayerTurn && this.playerDead()) || this.bossDead()
+}
+func (this Battle) bossDead() bool   { return this.BossHitPoints < 1 }
+func (this Battle) playerDead() bool { return this.PlayerHitPoints < 1 }
