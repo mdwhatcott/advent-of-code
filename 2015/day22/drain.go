0a31269dd461b0821e22ce90fe1811e81024c472
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-02 21:45:12 -0600

    Converted types to integers.

--- 2015/day22/drain.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Drain struct{}
-
-func (this Drain) Perform(state Battle) Battle {
-	return state
-}
--- 2015/day22/missile.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type Missile struct{}
-
-func (this *Missile) Perform(state Battle) Battle {
-	return state
-}
