65a24df357bfdf0f6a6ba9d40fc81120e29bd8d7
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-05 16:13:32 -0600

    Starting over on day 22 logic.

--- 2015/day22/main.go
+++ 2015/day22/main.go
@@ -11,49 +11,44 @@ func main() {
 }
 
 func part1() (min int) {
-	min = 1000000
-
-	battle := Battle{
-		IsPlayerTurn:    true,
-		PlayerHitPoints: 50,
-		PlayerMana:      250,
-		BossHitPoints:   71,
-		BossDamage:      10,
-	}
-
-	queue := []Battle{battle}
-
-	for len(queue) > 0 {
-
-		before := queue[0]
-		if len(queue) == 1 {
-			queue = nil
-		} else {
-			queue = queue[1:]
-		}
-
-		moves := before.Attack()
-		if len(moves) == 0 {
-			if before.BossHitPoints < 1 {
-				log.Printf("Player won: %+v", before)
-				min = before.PlayerManaSpent
-			} else {
-				log.Printf("Boss won: %+v", before)
-			}
-		}
-		for _, move := range moves {
-			after := before.Handle(move)
-
-			if !after.GameOver() {
-				queue = append(queue, after)
-			} else if after.BossHitPoints < 1 {
-				log.Printf("Player won: %+v", after)
-				min = after.PlayerManaSpent
-			} else {
-				log.Printf("Boss won: %+v", after)
-			}
-		}
-	}
-
-	return min
+	//min = 1000000
+	//
+	//battle := Battle{
+	//	IsPlayerTurn:    true,
+	//	PlayerHitPoints: 50,
+	//	PlayerMana:      250,
+	//	BossHitPoints:   71,
+	//	BossDamage:      10,
+	//}
+	//
+	//queue := []Battle{battle}
+	//
+	//for len(queue) > 0 {
+	//	before := queue[0]
+	//	queue = queue[1:]
+	//	moves := before.Attack()
+	//
+	//	if len(moves) == 0 {
+	//		if before.BossHitPoints < 1 {
+	//			log.Printf("Player won: %+v", before)
+	//			min = before.PlayerManaSpent
+	//		} else {
+	//			log.Printf("Boss won: %+v", before)
+	//		}
+	//	}
+	//	for _, move := range moves {
+	//		after := before.Handle(move)
+	//
+	//		if !after.GameOver() {
+	//			queue = append(queue, after)
+	//		} else if after.BossHitPoints < 1 {
+	//			log.Printf("Player won: %+v", after)
+	//			min = after.PlayerManaSpent
+	//		} else {
+	//			log.Printf("Boss won: %+v", after)
+	//		}
+	//	}
+	//}
+	//
+	//return min
 }
