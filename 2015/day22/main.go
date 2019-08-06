67c139fd19d42fad6e92ea0bba3f15343bf11891
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-06 14:49:41 -0600

    Rewrite, testing using the UI-log format. Still can't get the player to win...

--- 2015/day22/main.go
+++ 2015/day22/main.go
@@ -3,52 +3,61 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 )
 
 func main() {
 	log.SetFlags(0)
-	fmt.Println("Least mana spent:", part1())
+	fmt.Println("Least mana spent (expected: 1824) was...", part1())
 }
 
 func part1() (min int) {
-	//min = 1000000
-	//
-	//battle := Battle{
-	//	IsPlayerTurn:    true,
-	//	PlayerHitPoints: 50,
-	//	PlayerMana:      250,
-	//	BossHitPoints:   71,
-	//	BossDamage:      10,
-	//}
-	//
-	//queue := []Battle{battle}
-	//
-	//for len(queue) > 0 {
-	//	before := queue[0]
-	//	queue = queue[1:]
-	//	moves := before.Attack()
-	//
-	//	if len(moves) == 0 {
-	//		if before.BossHitPoints < 1 {
-	//			log.Printf("Player won: %+v", before)
-	//			min = before.PlayerManaSpent
-	//		} else {
-	//			log.Printf("Boss won: %+v", before)
-	//		}
-	//	}
-	//	for _, move := range moves {
-	//		after := before.Handle(move)
-	//
-	//		if !after.GameOver() {
-	//			queue = append(queue, after)
-	//		} else if after.BossHitPoints < 1 {
-	//			log.Printf("Player won: %+v", after)
-	//			min = after.PlayerManaSpent
-	//		} else {
-	//			log.Printf("Boss won: %+v", after)
-	//		}
-	//	}
-	//}
-	//
-	//return min
+	min = 999999999
+
+	battle := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 50,
+		PlayerMana:      250,
+		BossHitPoints:   71,
+		BossDamage:      10,
+	}
+
+	battles := 0
+	wins := 0
+	queue := []Battle{battle}
+
+	for len(queue) > 0 {
+		battle := queue[0]
+		queue = queue[1:]
+
+		var moves = bossAttack
+		if battle.IsPlayerTurn {
+			moves = allSpells
+		}
+		for _, move := range moves {
+			next, report := battle.TakeTurn(move)
+
+			if strings.Contains(report, "player wins") {
+				wins++
+				log.Println("Player!", strings.ReplaceAll(report, "\n", " | "))
+				if next.PlayerManaSpent < min {
+					log.Println("MIN!", next.PlayerManaSpent)
+					min = next.PlayerManaSpent
+				}
+				battles++
+			} else if strings.Contains(report, "boss wins") {
+				//log.Println("Boss!", strings.ReplaceAll(report, "\n", " | "))
+				battles++
+			} else if next.PlayerManaSpent < min {
+				queue = append(queue, next)
+			}
+		}
+	}
+	fmt.Println("Battles:", battles)
+	fmt.Println("Player wins:", wins)
+
+	return min
 }
+
+var bossAttack = []int{BossAttack}
+var allSpells = []int{Missile, Drain, Shield, Poison, Recharge}
