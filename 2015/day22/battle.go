091596c74ed94ba17b20f710f8b4fa8e1d567bdc
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-06 14:49:41 -0600

    Rewrite, testing using the UI-log format. Still can't get the player to win...

--- 2015/day22/battle.go
+++ 2015/day22/battle.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
+
 const (
 	BossAttack = iota
 	Missile
@@ -32,4 +38,117 @@ type Battle struct {
 	ShieldCounter   int
 	PoisonCounter   int
 	RechargeCounter int
+}
+
+func (this Battle) TakeTurn(attack int) (Battle, string) {
+	log := new(bytes.Buffer)
+
+	if this.IsPlayerTurn {
+		log.WriteString("-- Player turn --\n")
+	} else {
+		log.WriteString("-- Boss turn --\n")
+	}
+	fmt.Fprintf(log, "- Player has %d hit points, %d armor, %d mana\n", this.PlayerHitPoints, this.PlayerArmor, this.PlayerMana)
+	fmt.Fprintf(log, "- Boss has %d hit points\n", this.BossHitPoints)
+
+	this.IsPlayerTurn = !this.IsPlayerTurn
+
+	if this.ShieldCounter > 0 {
+		this.ShieldCounter--
+		fmt.Fprintf(log, "Shield's timer is now %d.\n", this.ShieldCounter)
+		if this.ShieldCounter == 0 {
+			fmt.Fprintf(log, "Shield wears off, decreasing armor by 7.\n")
+			this.PlayerArmor = 0
+		}
+	}
+
+	if this.PoisonCounter > 0 {
+		this.PoisonCounter--
+		this.BossHitPoints -= 3
+		fmt.Fprintf(log, "Poison deals 3 damage")
+		if this.BossHitPoints > 0 {
+			fmt.Fprintf(log, "; its timer is now %d.\n", this.PoisonCounter)
+		} else {
+			fmt.Fprintln(log, ". This kills the boss, and the player wins.")
+			return this, strings.TrimSpace(log.String())
+		}
+	}
+
+	if this.RechargeCounter > 0 {
+		this.RechargeCounter--
+		this.PlayerMana += 101
+		fmt.Fprintf(log, "Recharge provides 101 mana; its timer is now %d.\n", this.RechargeCounter)
+		if this.RechargeCounter == 0 {
+			fmt.Fprintln(log, "Recharge wears off.")
+		}
+	}
+
+	this.PlayerMana -= Cost[attack]
+	this.PlayerManaSpent += Cost[attack]
+
+	if this.PlayerMana < 1 {
+		fmt.Fprintf(log, "Player mana points depleted, and the boss wins.")
+		return this, strings.TrimSpace(log.String())
+	}
+
+	switch attack {
+
+	case BossAttack:
+		damage := this.BossDamage
+		armor := this.PlayerArmor
+		adjustedDamage := damage - armor
+		if adjustedDamage < 1 {
+			adjustedDamage = 1
+		}
+		if this.PlayerArmor > 0 {
+			fmt.Fprintf(log, "Boss attacks for %d - %d = %d damage!\n", this.BossDamage, this.PlayerArmor, adjustedDamage)
+		} else {
+			fmt.Fprintf(log, "Boss attacks for %d damage!\n", this.BossDamage)
+		}
+		this.PlayerHitPoints -= adjustedDamage
+
+	case Missile:
+		log.WriteString("Player casts Magic Missile, dealing 4 damage.")
+		this.BossHitPoints -= 4
+
+	case Drain:
+		log.WriteString("Player casts Drain, dealing 2 damage, and healing 2 hit points.")
+		this.BossHitPoints -= 2
+		this.PlayerHitPoints += 2
+
+	case Poison:
+		if this.PoisonCounter > 0 {
+			log.WriteString("Player casts Poison illegally, and the boss wins.")
+		} else {
+			log.WriteString("Player casts Poison.\n")
+			this.PoisonCounter = 6
+		}
+
+	case Shield:
+		if this.ShieldCounter > 0 {
+			log.WriteString("Player casts Shield illegally, and the boss wins.")
+		} else {
+			log.WriteString("Player casts Shield, increasing armor by 7.")
+			this.ShieldCounter += 6
+			this.PlayerArmor = 7
+		}
+
+	case Recharge:
+		if this.RechargeCounter > 0 {
+			log.WriteString("Player casts Recharge illegally, and the boss wins.")
+		} else {
+			log.WriteString("Player casts Recharge.\n")
+			this.RechargeCounter = 5
+		}
+	}
+
+	if this.BossHitPoints < 1 {
+		log.WriteString(" This kills the boss, and the player wins.")
+	} else if this.PlayerHitPoints < 1 {
+		log.WriteString(" This kills the player, and the boss wins.")
+	} else if this.IsPlayerTurn && this.PlayerMana < 53 && this.RechargeCounter == 0 {
+		fmt.Fprintf(log, " The player has only %d mana, and the boss wins.", this.PlayerMana)
+	}
+
+	return this, strings.TrimSpace(log.String())
 }
--- 2015/day22/battle_test.go
+++ 2015/day22/battle_test.go
@@ -1,1 +1,218 @@
 package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestBattleFixture(t *testing.T) {
+	gunit.Run(new(BattleFixture), t)
+}
+
+type BattleFixture struct {
+	*gunit.Fixture
+}
+
+func (this *BattleFixture) Test_Example1() {
+	a := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 10,
+		PlayerArmor:     0,
+		PlayerMana:      250,
+		PlayerManaSpent: 0,
+		BossHitPoints:   13,
+		BossDamage:      8,
+		ShieldCounter:   0,
+		PoisonCounter:   0,
+		RechargeCounter: 0,
+	}
+
+	b, B := a.TakeTurn(Poison)
+
+	this.So(b.IsPlayerTurn, should.BeFalse)
+	this.So(b.PlayerMana, should.Equal, 250-Cost[Poison])
+	this.So(b.PlayerManaSpent, should.Equal, Cost[Poison])
+	this.So(b.PoisonCounter, should.Equal, 6)
+	this.So(B, should.Equal, strings.TrimSpace(`
+-- Player turn --
+- Player has 10 hit points, 0 armor, 250 mana
+- Boss has 13 hit points
+Player casts Poison.
+`))
+
+	c, C := b.TakeTurn(BossAttack)
+
+	this.So(c.IsPlayerTurn, should.BeTrue)
+	this.So(c.PoisonCounter, should.Equal, 5)
+	this.So(c.BossHitPoints, should.Equal, 10)
+	this.So(c.PlayerHitPoints, should.Equal, 2)
+	this.So(C, should.Equal, strings.TrimSpace(`
+-- Boss turn --
+- Player has 10 hit points, 0 armor, 77 mana
+- Boss has 13 hit points
+Poison deals 3 damage; its timer is now 5.
+Boss attacks for 8 damage!
+`))
+
+	d, D := c.TakeTurn(Missile)
+
+	this.So(d.IsPlayerTurn, should.BeFalse)
+	this.So(d.PoisonCounter, should.Equal, 4)
+	this.So(d.BossHitPoints, should.Equal, 3)
+	this.So(d.PlayerMana, should.Equal, 250-Cost[Poison]-Cost[Missile])
+	this.So(d.PlayerManaSpent, should.Equal, Cost[Poison]+Cost[Missile])
+	this.So(D, should.Equal, strings.TrimSpace(`
+-- Player turn --
+- Player has 2 hit points, 0 armor, 77 mana
+- Boss has 10 hit points
+Poison deals 3 damage; its timer is now 4.
+Player casts Magic Missile, dealing 4 damage.
+`))
+
+	e, E := d.TakeTurn(BossAttack)
+
+	this.So(e.IsPlayerTurn, should.BeTrue)
+	this.So(e.PoisonCounter, should.Equal, 3)
+	this.So(e.BossHitPoints, should.Equal, 0)
+	this.So(e.PlayerHitPoints, should.Equal, 2)
+	this.So(e.PlayerMana, should.Equal, 250-Cost[Poison]-Cost[Missile])
+	this.So(e.PlayerManaSpent, should.Equal, Cost[Poison]+Cost[Missile])
+	this.So(E, should.Equal, strings.TrimSpace(`
+-- Boss turn --
+- Player has 2 hit points, 0 armor, 24 mana
+- Boss has 3 hit points
+Poison deals 3 damage. This kills the boss, and the player wins.
+`))
+}
+
+func (this *BattleFixture) Test_Example2() {
+	battle := Battle{
+		IsPlayerTurn:    true,
+		PlayerHitPoints: 10,
+		PlayerArmor:     0,
+		PlayerMana:      250,
+		PlayerManaSpent: 0,
+		BossHitPoints:   14,
+		BossDamage:      8,
+		ShieldCounter:   0,
+		PoisonCounter:   0,
+		RechargeCounter: 0,
+	}
+
+	for t, turn := range turns {
+		this.Println("Turn:", t)
+		var log string
+		battle, log = battle.TakeTurn(turn.Attack)
+		log = strings.ReplaceAll(log, "\n", " | ")
+		turn.ExpectedLog = strings.ReplaceAll(turn.ExpectedLog, "\n", " | ")
+		if !this.So(log, should.Equal, turn.ExpectedLog) {
+			break
+		}
+	}
+}
+
+var turns = []Example2Turn{
+	{
+		Attack: Recharge, // 0
+		ExpectedLog: "" +
+			"-- Player turn --" + "\n" +
+			"- Player has 10 hit points, 0 armor, 250 mana" + "\n" +
+			"- Boss has 14 hit points" + "\n" +
+			"Player casts Recharge.",
+	},
+	{
+		Attack: BossAttack, // 1
+		ExpectedLog: "" +
+			"-- Boss turn --" + "\n" +
+			"- Player has 10 hit points, 0 armor, 21 mana" + "\n" +
+			"- Boss has 14 hit points" + "\n" +
+			"Recharge provides 101 mana; its timer is now 4." + "\n" +
+			"Boss attacks for 8 damage!",
+	},
+	{
+		Attack: Shield, // 2
+		ExpectedLog: "" +
+			"-- Player turn --" + "\n" +
+			"- Player has 2 hit points, 0 armor, 122 mana" + "\n" +
+			"- Boss has 14 hit points" + "\n" +
+			"Recharge provides 101 mana; its timer is now 3." + "\n" +
+			"Player casts Shield, increasing armor by 7.",
+	},
+	{
+		Attack: BossAttack, // 3
+		ExpectedLog: "" +
+			"-- Boss turn --" + "\n" +
+			"- Player has 2 hit points, 7 armor, 110 mana" + "\n" +
+			"- Boss has 14 hit points" + "\n" +
+			"Shield's timer is now 5." + "\n" +
+			"Recharge provides 101 mana; its timer is now 2." + "\n" +
+			"Boss attacks for 8 - 7 = 1 damage!",
+	},
+	{
+		Attack: Drain, // 4
+		ExpectedLog: "" +
+			"-- Player turn --" + "\n" +
+			"- Player has 1 hit points, 7 armor, 211 mana" + "\n" +
+			"- Boss has 14 hit points" + "\n" +
+			"Shield's timer is now 4." + "\n" +
+			"Recharge provides 101 mana; its timer is now 1." + "\n" +
+			"Player casts Drain, dealing 2 damage, and healing 2 hit points.",
+	},
+	{
+		Attack: BossAttack, // 5
+		ExpectedLog: "" +
+			"-- Boss turn --" + "\n" +
+			"- Player has 3 hit points, 7 armor, 239 mana" + "\n" +
+			"- Boss has 12 hit points" + "\n" +
+			"Shield's timer is now 3." + "\n" +
+			"Recharge provides 101 mana; its timer is now 0." + "\n" +
+			"Recharge wears off." + "\n" +
+			"Boss attacks for 8 - 7 = 1 damage!",
+	},
+	{
+		Attack: Poison, // 6
+		ExpectedLog: "" +
+			"-- Player turn --" + "\n" +
+			"- Player has 2 hit points, 7 armor, 340 mana" + "\n" +
+			"- Boss has 12 hit points" + "\n" +
+			"Shield's timer is now 2." + "\n" +
+			"Player casts Poison.",
+	},
+	{
+		Attack: BossAttack, // 7
+		ExpectedLog: "" +
+			"-- Boss turn --" + "\n" +
+			"- Player has 2 hit points, 7 armor, 167 mana" + "\n" +
+			"- Boss has 12 hit points" + "\n" +
+			"Shield's timer is now 1." + "\n" +
+			"Poison deals 3 damage; its timer is now 5." + "\n" +
+			"Boss attacks for 8 - 7 = 1 damage!",
+	},
+	{
+		Attack: Missile, // 8
+		ExpectedLog: "" +
+			"-- Player turn --" + "\n" +
+			"- Player has 1 hit points, 7 armor, 167 mana" + "\n" +
+			"- Boss has 9 hit points" + "\n" +
+			"Shield's timer is now 0." + "\n" +
+			"Shield wears off, decreasing armor by 7." + "\n" +
+			"Poison deals 3 damage; its timer is now 4." + "\n" +
+			"Player casts Magic Missile, dealing 4 damage.",
+	},
+	{
+		Attack: BossAttack, // 9
+		ExpectedLog: "" +
+			"-- Boss turn --" + "\n" +
+			"- Player has 1 hit points, 0 armor, 114 mana" + "\n" +
+			"- Boss has 2 hit points" + "\n" +
+			"Poison deals 3 damage. This kills the boss, and the player wins.",
+	},
+}
+
+type Example2Turn struct {
+	Attack      int
+	ExpectedLog string
+}
