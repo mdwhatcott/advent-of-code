c8e44648858f959aa2deadfff16ea63413ad8495
Michael Whatcott <mdwhatcott@gmail.com>
2019-08-16 21:30:48 -0600

    2015 Day 22

--- 2015/day23/main.go
+++ 2015/day23/main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"advent/lib/util"
+)
+
 func main() {
+	part1()
+	part2()
+}
+
+func part1() {
+	interpreter := NewInterpreter(util.InputLines())
+	interpreter.ExecuteProgram()
+	fmt.Println("Part 1, Register B should be 255:", interpreter.registers["b"])
+}
+
+func part2() {
+	interpreter := NewInterpreter(util.InputLines())
+	interpreter.registers["a"] = 1
+	interpreter.ExecuteProgram()
+	fmt.Println("Part 2, Register B should be 334:", interpreter.registers["b"])
 }
