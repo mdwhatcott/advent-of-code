40a0e677ef6d4bf7f2b7a4038d445c0639f7ccc1
Michael Whatcott <mdwhatcott@gmail.com>
2019-11-29 21:22:46 -0700

    Progress on 2018 day 12, part 1

--- 2018/day12/pots_test.go
+++ 2018/day12/pots_test.go
@@ -1,6 +1,8 @@
 package day12
 
 import (
+	"bufio"
+	"strings"
 	"testing"
 
 	"github.com/smartystreets/assertions/should"
@@ -17,6 +19,76 @@ type StuffFixture struct {
 
 func (this *StuffFixture) TestSingleRule() {
 	rule := ParseRule("..... => #")
-	this.So(rule.Transform("....."), should.Equal, "#")
-	this.So(rule.Transform("#...."), should.Equal, ".")
+	this.So(rule.IsSatisfiedBy("....."), should.BeTrue)
+	this.So(rule.IsSatisfiedBy("#...."), should.BeFalse)
+	this.So(rule.String(), should.Equal, "#")
 }
+
+func (this *StuffFixture) TestRowSum() {
+	//                    0123456789012345678901234
+	pots := NewRowOfPots("#..#.#..##......###...###")
+	this.So(pots.Sum(), should.Equal, 145)
+}
+
+func (this *StuffFixture) TestFinalRowSum() {
+	//                    -- ++++++++++++++++++++++++
+	//                    210123456789012345678901234
+	pots := NewRowOfPots("#....##....#####...#######....#.#..##")
+	pots.min = -2
+	pots.max = len(pots.state) + pots.min
+	this.So(pots.Sum(), should.Equal, 325)
+}
+
+func (this *StuffFixture) TestRowTransformations() {
+	rules := []Rule{
+		ParseRule("...## => #"),
+		ParseRule("..#.. => #"),
+		ParseRule(".#... => #"),
+		ParseRule(".#.#. => #"),
+		ParseRule(".#.## => #"),
+		ParseRule(".##.. => #"),
+		ParseRule(".#### => #"),
+		ParseRule("#.#.# => #"),
+		ParseRule("#.### => #"),
+		ParseRule("##.#. => #"),
+		ParseRule("##.## => #"),
+		ParseRule("###.. => #"),
+		ParseRule("###.# => #"),
+		ParseRule("####. => #"),
+	}
+	pots := NewRowOfPots("#..#.#..##......###...###", rules...)
+	scanner := bufio.NewScanner(strings.NewReader(ExampleGenerations))
+	for scanner.Scan() {
+		pots.Scan()
+		pots.Update()
+		continue
+		this.So(pots.String(), should.Equal, this.extractExpectedPotGrowth(scanner.Text()))
+	}
+}
+
+func (this *StuffFixture) extractExpectedPotGrowth(text string) string {
+	line := strings.TrimSpace(text)
+	parts := strings.Fields(line)
+	return strings.Trim(parts[1], ".")
+}
+
+const ExampleGenerations = ` 1: ...#...#....#.....#..#..#..#...........
+ 2: ...##..##...##....#..#..#..##..........
+ 3: ..#.#...#..#.#....#..#..#...#..........
+ 4: ...#.#..#...#.#...#..#..##..##.........
+ 5: ....#...##...#.#..#..#...#...#.........
+ 6: ....##.#.#....#...#..##..##..##........
+ 7: ...#..###.#...##..#...#...#...#........
+ 8: ...#....##.#.#.#..##..##..##..##.......
+ 9: ...##..#..#####....#...#...#...#.......
+10: ..#.#..#...#.##....##..##..##..##......
+11: ...#...##...#.#...#.#...#...#...#......
+12: ...##.#.#....#.#...#.#..##..##..##.....
+13: ..#..###.#....#.#...#....#...#...#.....
+14: ..#....##.#....#.#..##...##..##..##....
+15: ..##..#..#.#....#....#..#.#...#...#....
+16: .#.#..#...#.#...##...#...#.#..##..##...
+17: ..#...##...#.#.#.#...##...#....#...#...
+18: ..##.#.#....#####.#.#.#...##...##..##..
+19: .#..###.#..#.#.#######.#.#.#..#.#...#..
+20: .#....##....#####...#######....#.#..##.`
