f5f227dbfe879489f9b4921dd5e31fb92c7da62f
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-02 08:02:08 -0700

    Solved 2019 day 1

--- 2019/day01/main.go
+++ 2019/day01/main.go
@@ -1,13 +1,32 @@
 package advent
 
 import (
+	"advent/lib/util"
 	_ "advent/lib/util"
 )
 
-func Part1() interface{} {
-	return nil
+func Part1() (sum int) {
+	for _, input := range util.InputInts("\n") {
+		fuel := int(float64(input)/3.0) - 2
+		sum = sum + fuel
+	}
+	return sum
 }
 
-func Part2() interface{} {
-	return nil
+func Part2() (sum int) {
+	for _, input := range util.InputInts("\n") {
+		module := 0
+		fuel := int(float64(input)/3.0) - 2
+		module = module + fuel
+
+		for {
+			fuel = int(float64(fuel)/3.0) - 2
+			if fuel < 0 {
+				break
+			}
+			module = module + fuel
+		}
+		sum = sum + module
+	}
+	return sum
 }
