24e45537055b8130fd275d16035f78ebd9b6632c
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-02 20:23:00 -0700

    Solved 2019 day 2 part 2

--- 2019/day02/main.go
+++ 2019/day02/main.go
@@ -14,5 +14,18 @@ func Part1() interface{} {
 }
 
 func Part2() interface{} {
-	return nil
+	original := util.InputInts(",")
+	program := make([]int, len(original))
+	for x := 0; x < 100; x++ {
+		for y := 0; y < 100; y++ {
+			copy(program, original)
+			program[1] = x
+			program[2] = y
+			RunProgram(program)
+			if program[0] == 19690720 {
+				return 100*x + y
+			}
+		}
+	}
+	panic("solution unknown")
 }
