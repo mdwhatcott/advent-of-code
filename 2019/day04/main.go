584c2f2ffc050b7dc471d115b53b644d963ac042
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-03 22:51:51 -0700

    Solved 2019 day 4

--- 2019/day04/main.go
+++ 2019/day04/main.go
@@ -1,13 +1,65 @@
 package advent
 
 import (
+	"fmt"
+	"strings"
+
+	"advent/lib/util"
 	_ "advent/lib/util"
 )
 
-func Part1() interface{} {
-	return nil
+func Part1() (count int) {
+	input := util.InputInts("-")
+	low, high := input[0], input[1]
+	for x := low; x < high; x++ {
+		num := fmt.Sprint(x)
+		if len(num) == 6 && hasPair(num) && !isDecreasing(num) {
+			count++
+		}
+	}
+	return count
 }
 
-func Part2() interface{} {
-	return nil
+func hasPair(num string) bool {
+	for y := 0; y < len(num)-1; y++ {
+		if num[y] == num[y+1] {
+			return true
+		}
+	}
+	return false
 }
+
+func isDecreasing(num string) bool {
+	for y := 0; y < len(num)-1; y++ {
+		if num[y+1] < num[y] {
+			return true
+		}
+	}
+	return false
+}
+func Part2() (count int) {
+	input := util.InputInts("-")
+	low, high := input[0], input[1]
+	for x := low; x < high; x++ {
+		num := fmt.Sprint(x)
+		if len(num) == 6 && !isDecreasing(num) && hasPair2(num) {
+			count++
+		}
+	}
+	return count
+}
+
+// This really should be a simple regular expression...
+func hasPair2(num string) bool {
+	for test := 0; test < len(pairs); test++ {
+		if strings.Contains(num, pairs[test]) && !strings.Contains(num, triplets[test]) {
+			return true
+		}
+	}
+	return false
+}
+
+var (
+	pairs    = []string{"00", "11", "22", "33", "44", "55", "66", "77", "88", "99"}
+	triplets = []string{"000", "111", "222", "333", "444", "555", "666", "777", "888", "999"}
+)
