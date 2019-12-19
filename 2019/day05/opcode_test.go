64745317c81f94734f0ad6fbe4231d9515c8e624
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-18 17:02:09 -0700

    Progress

--- 2019/day05/opcode_test.go
+++ 2019/day05/opcode_test.go
@@ -13,6 +13,20 @@ func TestOpCodeFixture(t *testing.T) {
 
 type OpCodeFixture struct {
 	*gunit.Fixture
+	i       int
+	inputs  []int
+	outputs []int
+}
+
+func (this *OpCodeFixture) input() int {
+	this.i++
+	return this.inputs[(this.i-1)%len(this.inputs)]
+}
+func (this *OpCodeFixture) output(value int) {
+	this.outputs = append(this.outputs, value)
+}
+func (this *OpCodeFixture) run(program []int) {
+	RunProgram(program, this.input, this.output)
 }
 
 func (this *OpCodeFixture) TestRunProgramA() {
@@ -22,9 +36,7 @@ func (this *OpCodeFixture) TestRunProgramA() {
 		99,
 		30, 40, 50,
 	}
-
-	RunProgram(program)
-
+	this.run(program)
 	this.So(program, should.Resemble, []int{
 		3500, 9, 10, 70,
 		2, 3, 11, 0,
@@ -33,59 +45,34 @@ func (this *OpCodeFixture) TestRunProgramA() {
 	})
 }
 func (this *OpCodeFixture) TestRunProgramB() {
-	program := []int{
-		1, 0, 0, 0, 99,
-	}
-
-	RunProgram(program)
-
-	this.So(program, should.Resemble, []int{
-		2, 0, 0, 0, 99,
-	})
+	program := []int{1, 0, 0, 0, 99}
+	this.run(program)
+	this.So(program, should.Resemble, []int{2, 0, 0, 0, 99})
 }
 func (this *OpCodeFixture) TestRunProgramC() {
-	program := []int{
-		2, 3, 0, 3, 99,
-	}
-
-	RunProgram(program)
-
-	this.So(program, should.Resemble, []int{
-		2, 3, 0, 6, 99,
-	})
+	program := []int{2, 3, 0, 3, 99}
+	this.run(program)
+	this.So(program, should.Resemble, []int{2, 3, 0, 6, 99})
 }
 func (this *OpCodeFixture) TestRunProgramD() {
-	program := []int{
-		2, 4, 4, 5, 99, 0,
-	}
-
-	RunProgram(program)
-
-	this.So(program, should.Resemble, []int{
-		2, 4, 4, 5, 99, 9801,
-	})
+	program := []int{2, 4, 4, 5, 99, 0}
+	this.run(program)
+	this.So(program, should.Resemble, []int{2, 4, 4, 5, 99, 9801})
 }
 func (this *OpCodeFixture) TestRunProgramE() {
-	program := []int{
-		1, 1, 1, 4, 99, 5, 6, 0, 99,
-	}
-
-	RunProgram(program)
-
-	this.So(program, should.Resemble, []int{
-		30, 1, 1, 4, 2, 5, 6, 0, 99,
-	})
+	program := []int{1, 1, 1, 4, 99, 5, 6, 0, 99}
+	this.run(program)
+	this.So(program, should.Resemble, []int{30, 1, 1, 4, 2, 5, 6, 0, 99})
 }
+
+func (this *OpCodeFixture) SkipTestRunProgramInputOutput() {
+	program := []int{3, 0, 4, 0, 99}
+	this.run(program)
+	this.So(program, should.Resemble, []int{3, 0, 4, 0, 99})
+}
+
 func (this *OpCodeFixture) SkipTestRunProgramF() {
-	program := []int{
-		1002, 4, 3, 4, 33,
-	}
-	RunProgram(program)
-
-	this.So(program, should.Resemble, []int{
-		1002, 4, 3, 4, 99,
-	})
-}
-func (this *OpCodeFixture) TestParseOpCodes() {
-	this.So(ParseOpCode([]int{}, 0), should.Resemble, OpCode{})
+	program := []int{1002, 4, 3, 4, 33}
+	this.run(program)
+	this.So(program, should.Resemble, []int{1002, 4, 3, 4, 99})
 }
