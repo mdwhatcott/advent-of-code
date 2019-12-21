991a962640d7537ee024bdc8068deb8d0d3c59ca
Michael Whatcott <mdwhatcott@gmail.com>
2019-12-20 23:52:03 -0700

    Solved 2019 day 5

--- 2019/day05/opcode.go
+++ 2019/day05/opcode.go
@@ -1,33 +1,50 @@
 package advent
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func RunProgram(program []int, input func() int, output func(int)) {
 	NewIntCodeInterpreter(program, input, output).RunProgram()
 }
 
 const (
-	AddInstruction      = 1
-	MultiplyInstruction = 2
-	InputInstruction    = 3
-	OutputInstruction   = 4
-	ExitInstruction     = 99
+	AddInstruction         = 1
+	MultiplyInstruction    = 2
+	InputInstruction       = 3
+	OutputInstruction      = 4
+	JumpIfTrueInstruction  = 5
+	JumpIfFalseInstruction = 6
+	LessThanInstruction    = 7
+	EqualsInstruction      = 8
+	ExitInstruction        = 99
 
 	PositionMode  = 0
 	ImmediateMode = 1
+
+	Slot1 = 2
+	Slot2 = 1
+	Slot3 = 0
 )
 
+var offsets = map[int]int{
+	Slot1: 1,
+	Slot2: 2,
+	Slot3: 3,
+}
+
 type IntCodeInterpreter struct {
-	program []int
 	pointer int
+	program []int
 	inputs  func() int
 	outputs func(int)
+	modes   []int
 }
 
 func NewIntCodeInterpreter(program []int, input func() int, output func(int)) *IntCodeInterpreter {
 	return &IntCodeInterpreter{
-		program: program,
 		pointer: 0,
+		program: program,
 		inputs:  input,
 		outputs: output,
 	}
@@ -35,56 +52,135 @@ func NewIntCodeInterpreter(program []int, input func() int, output func(int)) *I
 
 func (this *IntCodeInterpreter) RunProgram() {
 	for {
-		opCode := this.program[this.pointer]
-		switch opCode {
+		this.modes = splitDigits(this.value(this.pointer))
+		//log.Println(this.pointer, this.modes)
+
+		switch opCode(this.modes) {
+
 		case AddInstruction:
 			this.pointer += this.add()
+
 		case MultiplyInstruction:
 			this.pointer += this.multiply()
+
 		case InputInstruction:
 			this.pointer += this.input()
+
 		case OutputInstruction:
 			this.pointer += this.output()
+
+		case JumpIfTrueInstruction:
+			this.pointer = this.jumpIfTrue()
+
+		case JumpIfFalseInstruction:
+			this.pointer = this.jumpIfFalse()
+
+		case LessThanInstruction:
+			this.pointer += this.less()
+
+		case EqualsInstruction:
+			this.pointer += this.equals()
+
 		case ExitInstruction:
 			return
+
 		default:
-			panic(fmt.Sprintf("unknown opcode: %d", opCode))
+			panic(fmt.Sprintf("unknown instruction: %d", this.modes))
 		}
 	}
 }
 
-func (this *IntCodeInterpreter) SetValue(address, value int) {
+func (this *IntCodeInterpreter) access(slot int) int {
+	address := this.pointer + offsets[slot]
+	switch this.modes[slot] {
+
+	case PositionMode:
+		return this.reference(address)
+
+	case ImmediateMode:
+		return this.value(address)
+
+	default:
+		panic("not possible")
+	}
+}
+
+func (this *IntCodeInterpreter) value(address int) int {
+	return this.program[address]
+}
+func (this *IntCodeInterpreter) setValue(address, value int) {
 	this.program[address] = value
 }
 func (this *IntCodeInterpreter) setReference(address, value int) {
-	this.SetValue(this.GetValue(address), value)
-}
-func (this *IntCodeInterpreter) GetValue(address int) int {
-	return this.program[address]
+	this.setValue(this.value(address), value)
 }
 func (this *IntCodeInterpreter) reference(address int) int {
-	return this.GetValue(this.GetValue(address))
+	return this.value(this.value(address))
 }
 
 func (this *IntCodeInterpreter) add() int {
-	this.setReference(this.pointer+3,
-		this.reference(this.pointer+1)+
-			this.reference(this.pointer+2))
+	this.setReference(this.pointer+3, this.access(Slot1)+this.access(Slot2))
 	return 4
 }
 func (this *IntCodeInterpreter) multiply() int {
-	this.setReference(this.pointer+3,
-		this.reference(this.pointer+1)*
-			this.reference(this.pointer+2))
+	this.setReference(this.pointer+3, this.access(Slot1)*this.access(Slot2))
 	return 4
 }
-
 func (this *IntCodeInterpreter) input() int {
 	this.setReference(this.pointer+1, this.inputs())
 	return 2
 }
-
 func (this *IntCodeInterpreter) output() int {
-	this.outputs(this.reference(this.pointer+1))
+	this.outputs(this.access(Slot1))
 	return 2
+}
+func (this *IntCodeInterpreter) jumpIfTrue() int {
+	if this.access(Slot1) != 0 {
+		return this.access(Slot2)
+	}
+	return this.pointer + 3
+}
+func (this *IntCodeInterpreter) jumpIfFalse() int {
+	if this.access(Slot1) == 0 {
+		return this.access(Slot2)
+	}
+	return this.pointer + 3
+}
+func (this *IntCodeInterpreter) less() int {
+	if this.access(Slot1) < this.access(Slot2) {
+		this.setReference(this.pointer+3, 1)
+	} else {
+		this.setReference(this.pointer+3, 0)
+	}
+	return 4
+}
+func (this *IntCodeInterpreter) equals() int {
+	if this.access(Slot1) == this.access(Slot2) {
+		this.setReference(this.pointer+3, 1)
+	} else {
+		this.setReference(this.pointer+3, 0)
+	}
+	return 4
+}
+
+func opCode(digits []int) int {
+	if len(digits) == 1 {
+		return digits[0]
+	}
+	return digits[len(digits)-2]*10 + digits[len(digits)-1]
+}
+
+func splitDigits(value int) (digits []int) {
+	magnitude := 10
+	for value > 0 {
+		remainder := value % magnitude
+		digit := remainder / (magnitude / 10)
+		digits = append([]int{digit}, digits...)
+		value -= remainder
+		magnitude *= 10
+	}
+	for len(digits) < 5 {
+		digits = append([]int{0}, digits...)
+	}
+	return digits
 }
--- 2019/day05/opcode_test.go
+++ 2019/day05/opcode_test.go
@@ -65,14 +65,105 @@ func (this *OpCodeFixture) TestRunProgramE() {
 	this.So(program, should.Resemble, []int{30, 1, 1, 4, 2, 5, 6, 0, 99})
 }
 
-func (this *OpCodeFixture) SkipTestRunProgramInputOutput() {
+func (this *OpCodeFixture) TestRunProgramInputOutput() {
+	this.inputs = append(this.inputs, 42)
 	program := []int{3, 0, 4, 0, 99}
 	this.run(program)
-	this.So(program, should.Resemble, []int{3, 0, 4, 0, 99})
+	this.So(program, should.Resemble, []int{42, 0, 4, 0, 99})
+	this.So(this.outputs, should.Resemble, []int{42})
 }
 
-func (this *OpCodeFixture) SkipTestRunProgramF() {
+func (this *OpCodeFixture) TestRunProgramF() {
 	program := []int{1002, 4, 3, 4, 33}
 	this.run(program)
 	this.So(program, should.Resemble, []int{1002, 4, 3, 4, 99})
+}
+
+func (this *OpCodeFixture) TestRunProgramG() {
+	program := []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
+	this.inputs = append(this.inputs, 8)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{1})
+}
+
+func (this *OpCodeFixture) TestRunProgramG2() {
+	program := []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
+	this.inputs = append(this.inputs, 8+1)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{0})
+}
+
+func (this *OpCodeFixture) TestRunProgramH() {
+	program := []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}
+	this.inputs = append(this.inputs, 8)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{1})
+}
+
+func (this *OpCodeFixture) TestRunProgramH2() {
+	program := []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}
+	this.inputs = append(this.inputs, 8+1)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{0})
+}
+
+func (this *OpCodeFixture) TestRunProgramI() {
+	program := []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
+	this.inputs = append(this.inputs, 0)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{0})
+}
+
+func (this *OpCodeFixture) TestRunProgramI1() {
+	program := []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
+	this.inputs = append(this.inputs, 1)
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{1})
+}
+
+func (this *OpCodeFixture) TestFinalProgramA() {
+	program := []int{
+		3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99,
+	}
+	this.inputs = []int{8 - 1}
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{999})
+}
+
+func (this *OpCodeFixture) TestFinalProgramB() {
+	program := []int{
+		3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99,
+	}
+	this.inputs = []int{8 - 0}
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{1000})
+}
+
+func (this *OpCodeFixture) SkipTestFinalProgramC() {
+	program := []int{
+		3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99,
+	}
+	this.inputs = []int{8 - 1}
+	this.run(program)
+	this.So(this.outputs, should.Resemble, []int{1001})
+}
+
+func (this *OpCodeFixture) TestParsing() {
+	this.So(splitDigits(5), should.Resemble, []int{0, 0, 0, 0, 5})
+	this.So(splitDigits(45), should.Resemble, []int{0, 0, 0, 4, 5})
+	this.So(splitDigits(345), should.Resemble, []int{0, 0, 3, 4, 5})
+	this.So(splitDigits(2345), should.Resemble, []int{0, 2, 3, 4, 5})
+	this.So(splitDigits(12345), should.Resemble, []int{1, 2, 3, 4, 5})
+
+	this.So(opCode(splitDigits(5)), should.Equal, 5)
+	this.So(opCode(splitDigits(45)), should.Equal, 45)
+	this.So(opCode(splitDigits(345)), should.Equal, 45)
+	this.So(opCode(splitDigits(2345)), should.Equal, 45)
+	this.So(opCode(splitDigits(12345)), should.Equal, 45)
 }
