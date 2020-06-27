f9b7f81bf588692d733ce9bb64c46d61988b3a67
Michael Whatcott <mdwhatcott@gmail.com>
2020-06-26 20:50:19 -0600

    Working on 2019 day 7 part 2

--- 2019/day07/stuff_test.go
+++ 2019/day07/stuff_test.go
@@ -15,20 +15,40 @@ type StuffFixture struct {
 	*gunit.Fixture
 }
 
-func (this *StuffFixture) Test1() {
+func (this *StuffFixture) TestPart1_Example1() {
 	runner := NewIO(4, 3, 2, 1, 0)
 	answer := runner.Run(3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0)
 	this.So(answer, should.Equal, 43210)
 }
 
-func (this *StuffFixture) Test2() {
+func (this *StuffFixture) TestPart1_Example2() {
 	runner := NewIO(0, 1, 2, 3, 4)
 	answer := runner.Run(3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0)
 	this.So(answer, should.Equal, 54321)
 }
 
-func (this *StuffFixture) Test3() {
+func (this *StuffFixture) TestPart1_Example3() {
 	runner := NewIO(1, 0, 4, 3, 2)
 	answer := runner.Run(3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0)
 	this.So(answer, should.Equal, 65210)
+}
+
+func (this *StuffFixture) TestPart2_Example1() {
+	answer := part2(
+		[]int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+		9, 8, 7, 6, 5,
+	)
+	this.So(answer, should.Equal, 139629729)
+}
+
+func (this *StuffFixture) SkipTestPart2_Example2() { // TODO: this deadlocks
+	answer := part2(
+		[]int{
+			3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54,
+			-5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4,
+			53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10,
+		},
+		9, 7, 8, 5, 6,
+	)
+	this.So(answer, should.Equal, 18216)
 }
