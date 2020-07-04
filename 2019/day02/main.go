69680ac6c65a8b0d3f1b5a9282b9b5fcd6a0d198
Michael Whatcott <mdwhatcott@gmail.com>
2020-07-03 20:54:42 -0600

    Fixed day 2

--- 2019/day02/main.go
+++ 2019/day02/main.go
@@ -7,8 +7,7 @@ import (
 
 func Part1() interface{} {
 	program := util.InputInts(",")
-	RunTweakedProgram(program, 12, 2)
-	return program[0]
+	return RunTweakedProgram(program, 12, 2)[0]
 }
 
 func Part2() interface{} {
@@ -17,8 +16,8 @@ func Part2() interface{} {
 	for x := 0; x < 100; x++ {
 		for y := 0; y < 100; y++ {
 			copy(program, original)
-			RunTweakedProgram(program, x, y)
-			if program[0] == 19690720 {
+			tweaked := RunTweakedProgram(program, x, y)
+			if tweaked[0] == 19690720 {
 				return 100*x + y
 			}
 		}
@@ -26,11 +25,11 @@ func Part2() interface{} {
 	panic("solution unknown")
 }
 
-func RunTweakedProgram(program []int, noun, verb int) {
+func RunTweakedProgram(program []int, noun, verb int) []int {
 	program[1] = noun
 	program[2] = verb
-	RunProgram(program)
+	return RunProgram(program)
 }
-func RunProgram(program []int) {
-	intcode.RunProgram(program, nil, nil)
+func RunProgram(program []int) []int {
+	return intcode.RunProgram(program, nil, nil)
 }
