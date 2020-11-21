51cebcf7b0e1756c5d1ee51ee5bd7c1e69d76c8c
Michael Whatcott <mdwhatcott@gmail.com>
2020-11-21 15:42:51 -0700

    Solved 2015 day 24

--- 2015/day24/combinations.go
+++ 2015/day24/combinations.go
@@ -1,11 +1,36 @@
 package main
 
-func product(iterable []interface{}, repeat int) chan []interface{} {
-	return nil
+func product(items ...int) (result int) {
+	result = items[0]
+	for _, item := range items[1:] {
+		result *= item
+	}
+	return result
 }
 
-func combinations(iterable []interface{}, length int) chan []interface{} {
-	stream := make(chan []interface{})
-	//go _combinations(stream, iterable, length)
+func combinations(iterable []int, count int) chan []int {
+	stream := make(chan []int)
+	go combinationGenerator(iterable, count, stream)
 	return stream
+}
+
+// Credit: http://rosettacode.org/wiki/Combinations#Go
+func combinationGenerator(source []int, count int, out chan []int) {
+	working := make([]int, count)
+	last := count - 1
+	var generate func(int, int)
+	generate = func(i, next int) {
+		for j := next; j < len(source); j++ {
+			working[i] = source[j]
+			if i == last {
+				combination := make([]int, len(working))
+				copy(combination, working)
+				out <- combination
+			} else {
+				generate(i+1, j+1)
+			}
+		}
+	}
+	generate(0, 0)
+	close(out)
 }
