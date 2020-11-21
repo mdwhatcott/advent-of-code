1653056350709f73924f7e10f2d0d6cb457e3f90
Michael Whatcott <mdwhatcott@gmail.com>
2020-11-21 15:44:53 -0700

    Consolidated code.

--- 2015/day24/main.go
+++ 2015/day24/main.go
@@ -39,3 +39,45 @@ func arrangePresentsOnSleigh(compartments int) {
 
 	fmt.Println(quantumEntanglement)
 }
+
+func sum(values ...int) (sum int) {
+	for _, value := range values {
+		sum += value
+	}
+	return sum
+}
+
+func product(items ...int) (result int) {
+	result = items[0]
+	for _, item := range items[1:] {
+		result *= item
+	}
+	return result
+}
+
+func combinations(iterable []int, count int) chan []int {
+	stream := make(chan []int)
+	go combinationGenerator(iterable, count, stream)
+	return stream
+}
+
+// Credit: http://rosettacode.org/wiki/Combinations#Go
+func combinationGenerator(source []int, count int, out chan []int) {
+	working := make([]int, count)
+	last := count - 1
+	var generate func(int, int)
+	generate = func(i, next int) {
+		for j := next; j < len(source); j++ {
+			working[i] = source[j]
+			if i == last {
+				combination := make([]int, len(working))
+				copy(combination, working)
+				out <- combination
+			} else {
+				generate(i+1, j+1)
+			}
+		}
+	}
+	generate(0, 0)
+	close(out)
+}
