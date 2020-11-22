562f894912430ed2a0f8b32c20b75b53b89bd21d
Michael Whatcott <mdwhatcott@gmail.com>
2020-11-21 21:00:35 -0700

    Solved 2018 day 12

--- 2018/day12/main.go
+++ 2018/day12/main.go
@@ -1,9 +1,55 @@
 package day12
 
+import (
+	"fmt"
+	"strings"
+
+	"advent/lib/util"
+)
+
 func Part1() interface{} {
-	return nil
+	lines := util.InputLines()
+	initial := strings.Fields(lines[0])[2]
+	var rules []Rule
+	for _, line := range lines[2:] {
+		rules = append(rules, ParseRule(line))
+	}
+	return NewRowOfPots(initial, rules...).Grow(20)
 }
 
 func Part2() interface{} {
-	return nil
+	lines := util.InputLines()
+	initial := strings.Fields(lines[0])[2]
+	var rules []Rule
+	for _, line := range lines[2:] {
+		rules = append(rules, ParseRule(line))
+	}
+
+	oldDiff := 0
+	diffRepeats := 0
+	x := 0
+	old := 0
+
+	row := NewRowOfPots(initial, rules...)
+
+	for {
+		x++
+		row.Update(row.Scan())
+		sum := row.Sum()
+
+		// Wait for the diff between sums of subsequent generations to stabilize, then calculate the end result.
+		diff := sum - old
+		if diff == oldDiff {
+			diffRepeats++
+		} else {
+			oldDiff = diff
+			diffRepeats = 0
+		}
+		old = sum
+
+		if diffRepeats > 10 {
+			fmt.Println(x, sum)
+			return sum + (diff * (50000000000 - x))
+		}
+	}
 }
--- 2018/day12/pots.go
+++ 2018/day12/pots.go
@@ -1,113 +1,92 @@
 package day12
 
 import (
-	"bytes"
 	"strings"
 )
 
-type Rule struct {
-	pots   string
-	result string
-}
-
-func ParseRule(raw string) Rule {
-	parts := strings.Split(raw, " => ")
-	return Rule{
-		pots:   parts[0],
-		result: parts[1],
-	}
-}
-
-func (this Rule) String() string {
-	return this.result
-}
-
-func (this Rule) IsSatisfiedBy(pots string) bool {
-	return pots == this.pots
-}
-
 type Row struct {
-	min    int
-	max    int
-	state  string
-	rules  []Rule
-	buffer *bytes.Buffer
+	state map[int]bool
+	rules []Rule
 }
 
 func NewRowOfPots(initial string, rules ...Rule) *Row {
+	state := make(map[int]bool, len(initial))
+	for i, c := range initial {
+		state[i] = c == '#'
+	}
 	return &Row{
-		min:    0,
-		max:    len(initial),
-		state:  initial,
-		rules:  rules,
-		buffer: new(bytes.Buffer),
+		state: state,
+		rules: rules,
 	}
 }
 
-func (this *Row) Scan() {
-	this.buffer.WriteString(this.state)
-	for p, pot := range this.state {
-		var neighbors string
+func (this *Row) Scan() (result map[int]bool) {
+	result = make(map[int]bool)
 
-		if p == 0 {
-			neighbors += ".."
-			this.min -= 2
-		} else if p == 1 {
-			neighbors += "."
-			this.min -= 1
-		}
-
-		neighbors += string(pot)
-
-		if p == len(this.state)-1 {
-			neighbors += "."
-			this.max += 1
-		} else if p == len(this.state)-2 {
-			neighbors += ".."
-			this.max += 2
-		}
-
-		before := this.buffer.Len()
-
+	min, max := this.MinMax()
+	for pot := min - 5; pot < max+5; pot++ {
 		for _, rule := range this.rules {
-			if rule.IsSatisfiedBy(neighbors) {
-				this.buffer.WriteString(rule.String())
-				break
+			if rule.IsSatisfiedBy(this.neighbors(pot)) {
+				result[pot] = rule.result == "#"
 			}
 		}
-
-		if this.buffer.Len() == before {
-			this.buffer.WriteRune(pot)
-		}
 	}
-	/*
-		for pot in state:
-			if pot is near the edge:
-				fill in adjacent (empty) neighbor pots
-					remember to adjust min/max accordingly...
-			for rule in rules: // optimization: match the neighbors to rules by traversing a tree...
-				if rule.IsSatisfiedBy(neighbors):
-					write rule.String() to buffer
-					break
-			else:
-				write current pot to buffer
-	*/
+
+	return result
+}
+func (this *Row) neighbors(pot int) string {
+	return "" +
+		this.at(pot-2) +
+		this.at(pot-1) +
+		this.at(pot+0) +
+		this.at(pot+1) +
+		this.at(pot+2)
+}
+func (this *Row) at(i int) string {
+	if this.state[i] {
+		return "#"
+	} else {
+		return "."
+	}
 }
 
-func (this *Row) Update() {
-	this.state = this.buffer.String()
-	this.buffer.Reset()
+func (this *Row) Update(scan map[int]bool) {
+	this.state = scan
 }
 
 func (this *Row) Sum() (sum int) {
-	for x, value := 0, this.min; value < this.max; x, value = x+1, value+1 {
-		if this.state[x:x+1] == "#" {
-			sum += value
+	for pot, plant := range this.state {
+		if plant {
+			sum += pot
 		}
 	}
 	return sum
 }
 
 func (this *Row) Render() string {
-	return ""
+	min, max := this.MinMax()
+	builder := new(strings.Builder)
+	for x := min; x <= max; x++ {
+		builder.WriteString(this.at(x))
+	}
+	return builder.String()
+}
+
+func (this *Row) MinMax() (min, max int) {
+	for pot := range this.state {
+		if pot < min {
+			min = pot
+		}
+		if pot > max {
+			max = pot
+		}
+	}
+	return min, max
+}
+
+func (row *Row) Grow(generations int) interface{} {
+	for x := 0; x < generations; x++ {
+		row.Update(row.Scan())
+	}
+	return row.Sum()
 }
