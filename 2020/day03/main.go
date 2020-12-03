50f0bef061d4459d920b95f423180334808aae7a
Michael Whatcott <mdwhatcott@gmail.com>
2020-12-02 23:33:05 -0700

    Solved 2020 day 3

--- 2020/day03/main.go
+++ 2020/day03/main.go
@@ -1,9 +1,32 @@
 package advent
 
-func Part1() interface{} {
-	return nil
+import "advent/lib/util"
+
+func Part1() int {
+	return Forest(util.InputLines()).CountTreesHitOnDescent(1, 3)
 }
 
-func Part2() interface{} {
-	return nil
+func Part2() int {
+	forest := Forest(util.InputLines())
+	return forest.CountTreesHitOnDescent(1, 1) *
+		forest.CountTreesHitOnDescent(1, 3) *
+		forest.CountTreesHitOnDescent(1, 5) *
+		forest.CountTreesHitOnDescent(1, 7) *
+		forest.CountTreesHitOnDescent(2, 1)
 }
+
+type Forest []string
+
+func (this Forest) CountTreesHitOnDescent(rise, run int) (hits int) {
+	row, col := 0, 0
+	for row < len(this) {
+		if this[row][col] == tree {
+			hits++
+		}
+		row += rise
+		col = (col + run) % len(this[0])
+	}
+	return hits
+}
+
+const tree = '#'
