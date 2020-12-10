1c0050854c9e545f1cb16bfb6fe842d10a78b0ba
Michael Whatcott <mdwhatcott@gmail.com>
2020-12-09 19:56:49 -0700

    Solved 2020 day 9

--- 2020/day09/main.go
+++ 2020/day09/main.go
@@ -1,9 +1,45 @@
 package advent
 
-func Part1() interface{} {
-	return nil
+import (
+	"advent/lib/util"
+)
+
+func Part1() int {
+	numbers := util.InputInts("\n")
+	for x := 26; x < len(numbers); x++ {
+		if !canSum(numbers[x-26 : x+1]...) {
+			return numbers[x]
+		}
+	}
+	panic("boink")
 }
 
 func Part2() interface{} {
-	return nil
+	numbers := util.InputInts("\n")
+	search := Part1()
+	for x := 0; x < len(numbers); x++ {
+		sum := 0
+		for y := x + 1; sum < search; y++ {
+			sum += numbers[y]
+			if sum == search {
+				return util.Min(numbers[x:y+1]...) + util.Max(numbers[x:y+1]...)
+			}
+		}
+	}
+	panic("boink")
+}
+
+func canSum(numbers ...int) bool {
+	search := numbers[len(numbers)-1]
+	for x := 0; x < len(numbers); x++ {
+		for y := x + 1; y < len(numbers)-1; y++ {
+			if numbers[x] == numbers[y] {
+				continue
+			}
+			if numbers[x]+numbers[y] == search {
+				return true
+			}
+		}
+	}
+	return false
 }
