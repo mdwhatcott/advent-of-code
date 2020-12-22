2f2f4250b0c5717dee9668de0de271ab92eaa562
Michael Whatcott <mdwhatcott@gmail.com>
2020-12-21 22:50:40 -0700

    Solved 2020 day 15

--- 2020/day15/main.go
+++ 2020/day15/main.go
@@ -1,9 +1,74 @@
 package advent
 
+import (
+	"container/list"
+
+	"advent/lib/util"
+)
+
 func Part1() interface{} {
-	return nil
+	return part1(2020, util.InputInts(",")...)
+}
+
+func part1(until int, inputs ...int) interface{} {
+	game := list.New()
+
+	for _, input := range inputs {
+		game.PushFront(input)
+	}
+
+	for game.Len() < until {
+		next := sinceLastOccurrence(game.Front())
+		game.PushFront(next)
+	}
+
+	return game.Front().Value
+}
+
+func sinceLastOccurrence(cursor *list.Element) int {
+	search := cursor.Value
+	for x := 1; ; x++ {
+		cursor = cursor.Next()
+		if cursor == nil {
+			return 0
+		}
+		if cursor.Value == search {
+			return x
+		}
+	}
 }
 
 func Part2() interface{} {
-	return nil
+	return part2(30000000, util.InputInts(",")...)
+}
+
+func part2(until int, inputs ...int) interface{} {
+	game := list.New()
+	memory := make(map[interface{}][]int) // map[num][]occurrences
+
+	for _, input := range inputs {
+		game.PushFront(input)
+		memory[input] = append(memory[input], game.Len())
+	}
+
+	for game.Len() < until {
+		next := sinceLastOccurrenceCache(memory, game.Front())
+		game.PushFront(next)
+		memory[next] = append(memory[next], game.Len())
+	}
+
+	return game.Front().Value
+}
+
+func sinceLastOccurrenceCache(memory map[interface{}][]int, front *list.Element) int {
+	values := memory[front.Value]
+	if len(values) == 1 {
+		return 0
+	}
+
+	if len(values) > 2 {
+		values = values[len(values)-2:]
+	}
+
+	return values[len(values)-1] - values[len(values)-2]
 }
--- 2020/day15/main_test.go
+++ 2020/day15/main_test.go
@@ -1,1 +1,12 @@
 package advent
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions"
+	"github.com/smartystreets/assertions/should"
+)
+
+func TestPart1Examples(t *testing.T) {
+	assertions.New(t).So(part1(2020, 0, 3, 6), should.Equal, 436)
+}
