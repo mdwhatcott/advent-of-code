3cfca95c8ea4a067f48a88391e64f54bfaefb36b
Michael Whatcott <mdwhatcott@gmail.com>
2021-01-05 22:29:13 -0700

    First steps on 2015 day 6

--- rkt/2015/06.rkt
+++ rkt/2015/06.rkt
@@ -8,4 +8,58 @@
          racket/set
          rackunit)
 
-(test-equal? "01" 'a 'a)
+(define INPUT
+  (string-split (file->string "06.txt") "\n"))
+
+(define (parse-action line)
+  (cond [(string-contains? line "off")    0]
+        [(string-contains? line "on")     1]
+        [(string-prefix?   line "toggle") 2]))
+
+(define (parse-numerals line)
+  (map add1
+    (map string->number
+      (string-split
+        (string-replace
+          (string-replace
+            (string-replace
+              (string-replace
+                (string-replace
+                  (string-replace line
+                    "toggle " "")
+                  "turn " "")
+                "off " "")
+              "on " "")
+            "through " "")
+          "," " ")))))
+
+(define (parse-from line)
+  1)
+
+(define (parse-to line)
+  4)
+
+(define (parse-instruction line)
+  (list (parse-action line)
+        (parse-numerals line)))
+
+; (define (apply-instruction all instruction)
+  ; (for/??))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(test-equal? "parse-instruction (off = 0)" 
+  (parse-instruction "turn off 0,1 through 2,3")
+  (list 0 (list 1 2 3 4)))
+
+(test-equal? "parse-instruction (on = 1)" 
+  (parse-instruction "turn on 0,1 through 2,3")
+  (list 1 (list 1 2 3 4)))
+
+(test-equal? "parse-instruction (toggle = 2)" 
+  (parse-instruction "toggle 0,1 through 2,3")
+  (list 2 (list 1 2 3 4)))
+
+; (test-equal? "apply-instruction"
+  ; (apply-instruction (hash) (list 1 (list 1 1 2 2)))
+  ; (hash))
