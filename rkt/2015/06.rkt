bdea5a014ad9ee6d05af55d7c65210734a606113
Michael Whatcott <mdwhatcott@gmail.com>
2021-01-06 17:19:42 -0700

    Simplified things.

--- rkt/2015/06.rkt
+++ rkt/2015/06.rkt
@@ -3,11 +3,8 @@
 #lang racket/base
 
 (require racket/file
-         racket/list
          racket/string
-         racket/set
-         racket/stream
-         rackunit)
+         racket/stream)
 
 (define INPUT
   (string-split (file->string "06.txt") "\n"))
@@ -34,35 +31,23 @@
             "through " "")
           "," " ")))))
 
-(define (parse-instruction line)
-  (list (parse-action line)
-        (parse-numerals line)))
-
 (define (light-range xyxy)
   (in-range (* (list-ref xyxy 0) (list-ref xyxy 1))
       (add1 (* (list-ref xyxy 2) (list-ref xyxy 3)))))
 
-; (define (apply-instruction all instruction)
-  ; (for/??))
+(define (apply-instruction lights instruction)
+  lights)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require rackunit)
 
 (test-equal? "light-range"
   (stream->list (light-range (list 1 2 3 4))) ; (* 1 2) ... (* 3 4)
   (list 2 3 4 5 6 7 8 9 10 11 12))            ;    2    ...    12
 
-(test-equal? "parse-instruction (off = 0)" 
-  (parse-instruction "turn off 0,1 through 2,3")
-  (list 0 (list 1 2 3 4)))
+(test-equal? "parse-action" (parse-action "turn off") 0)
+(test-equal? "parse-action" (parse-action "turn on")  1)
+(test-equal? "parse-action" (parse-action "toggle")   2)
 
-(test-equal? "parse-instruction (on = 1)" 
-  (parse-instruction "turn on 0,1 through 2,3")
-  (list 1 (list 1 2 3 4)))
-
-(test-equal? "parse-instruction (toggle = 2)" 
-  (parse-instruction "toggle 0,1 through 2,3")
-  (list 2 (list 1 2 3 4)))
-
-; (test-equal? "apply-instruction"
-  ; (apply-instruction (hash) (list 1 (list 1 1 2 2)))
-  ; (hash))
+(test-equal? "apply-instruction"
+  (apply-instruction (hash) "turn on 0,0 through 1,1")
+  (hash))
