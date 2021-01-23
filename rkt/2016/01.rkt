5db1a7ab8b77cd753a42bdc4d1272b12acf5fd75
Michael Whatcott <mdwhatcott@gmail.com>
2021-01-23 11:12:44 -0700

    More goofing around with 2016 day 1 in racket.

--- rkt/2016/01.rkt
+++ rkt/2016/01.rkt
@@ -6,30 +6,28 @@
          racket/list
          racket/string)
 
-(define UP    (cons 0 1))
-(define DOWN  (cons 0 -1))
-(define LEFT  (cons -1 0))
-(define RIGHT (cons 1 0))
+(define N (cons 0 1))
+(define S (cons 0 -1))
+(define W (cons -1 0))
+(define E (cons 1 0))
 
-(define (turn-right from)
-  (cond ((eq? from UP)    RIGHT)
-        ((eq? from RIGHT) DOWN)
-        ((eq? from DOWN)  LEFT)
-        ((eq? from LEFT)  UP)))
+(define (R from)
+  (cond ((eq? from N) E)
+        ((eq? from E) S)
+        ((eq? from S) W)
+        ((eq? from W) N)))
 
-(define (turn-left from)
-  (turn-right
-    (turn-right
-      (turn-right from))))
+(define (L from)
+  (R (R (R from))))
+
+(define (turn from instruction)
+  (if (eq? (string-ref instruction 0) #\R)
+      (R from)
+      (L from)))
 
 (define (walk direction from)
   (cons (+ (car from) (car direction))
         (+ (cdr from) (cdr direction))))
-
-(define (parse-turn d)
-  (if (eq? (string-ref d 0) #\R)
-      turn-right
-      turn-left))
 
 (define (parse-distance d)
   (string->number (substring d 1)))
@@ -40,21 +38,24 @@
 
 (define (travel step facing from)
   (walk-n (parse-distance step)
-          ((parse-turn step) facing)
+          (turn facing step)
           from))
 
-(define (travel-all facing from steps)
-  (if (empty? steps) from
-    (travel-all ((parse-turn (first steps)) facing)
-                (travel (first steps) facing from)
-                (rest steps))))
+(define (travel-all steps)
+  (let loop ([facing N]
+             [from (cons 0 0)]
+             [steps steps])
+    (if (empty? steps) from
+      (loop (turn facing (first steps))
+            (travel (first steps) facing from)
+            (rest steps)))))
 
 (define (pair2list p)
   (list (car p) (cdr p)))
 
 (define (part1 input)
   (apply + (map abs (pair2list
-    (travel-all UP (cons 0 0) (string-split input ", "))))))
+    (travel-all (string-split input ", "))))))
 
 (define INPUT (file->string "01.txt"))
 
@@ -67,31 +68,43 @@
 
 (require rackunit)
 
-(test-equal? "turn-right" (turn-right UP)    RIGHT)
-(test-equal? "turn-right" (turn-right RIGHT) DOWN)
-(test-equal? "turn-right" (turn-right DOWN)  LEFT)
-(test-equal? "turn-right" (turn-right LEFT)  UP)
+(test-equal? "R" (R N) E)
+(test-equal? "R" (R E) S)
+(test-equal? "R" (R S) W)
+(test-equal? "R" (R W) N)
 
-(test-equal? "turn-left"  (turn-left  UP)    LEFT)
-(test-equal? "turn-left"  (turn-left  LEFT)  DOWN)
-(test-equal? "turn-left"  (turn-left  DOWN)  RIGHT)
-(test-equal? "turn-left"  (turn-left  RIGHT) UP)
+(test-equal? "L" (L N) W)
+(test-equal? "L" (L W) S)
+(test-equal? "L" (L S) E)
+(test-equal? "L" (L E) N)
 
-(test-equal? "walk" (walk UP    (cons 2 2)) (cons 2 3))
-(test-equal? "walk" (walk DOWN  (cons 2 2)) (cons 2 1))
-(test-equal? "walk" (walk LEFT  (cons 2 2)) (cons 1 2))
-(test-equal? "walk" (walk RIGHT (cons 2 2)) (cons 3 2))
-
-(test-equal? "parse-turn" (parse-turn "R2") turn-right)
-(test-equal? "parse-turn" (parse-turn "L2") turn-left)
+(test-equal? "walk" (walk N (cons 2 2)) (cons 2 3))
+(test-equal? "walk" (walk S (cons 2 2)) (cons 2 1))
+(test-equal? "walk" (walk W (cons 2 2)) (cons 1 2))
+(test-equal? "walk" (walk E (cons 2 2)) (cons 3 2))
 
 (test-equal? "parse-distance" (parse-distance "R32") 32)
 (test-equal? "parse-distance" (parse-distance "L6") 6)
 
-(test-equal? "walk-n" (walk-n 10 RIGHT (cons 0 0)) (cons 10 0))
+(test-equal? "walk-n" (walk-n 10 E (cons 0 0)) (cons 10 0))
 
-(test-equal? "travel" (travel "R10" UP (cons 0 0)) (cons 10 0))
+(test-equal? "travel" (travel "R10" N (cons 0 0)) (cons 10 0))
 
 (test-equal? "travel-all"
-  (travel-all UP (cons 0 0) (list "R5" "L5" "R5" "R3"))
+  (travel-all (list "R5" "L5" "R5" "R3"))
   (cons 10 2))
+
+(define (enumerate-steps raw)
+  ; (for/fold ([steps (list)])
+            ; ([raw-step raw])
+    ; (append steps
+      ; (for/list ([])))))
+  (list E E E N W W S S W N N N E S S S))
+
+(test-equal? "enumerate-steps"
+  (enumerate-steps (list "R3" "L1" "L2" "L2" "R1" "R3" "R1" "R3"))
+  (list E E E N W W S S W N N N E S S S))
+
+
+; R3 L1 L2 L2 R1 R3 R1 R3
+; E E E N W W S S W N N N E S S S
