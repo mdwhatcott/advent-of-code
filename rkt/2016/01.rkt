c67f665f7770f510c263295df47d167da80b0509
Michael Whatcott <mdwhatcott@gmail.com>
2021-01-23 20:27:24 -0700

    Solved 2016 day 1 in racket

--- rkt/2016/01.rkt
+++ rkt/2016/01.rkt
@@ -4,7 +4,10 @@
 
 (require racket/file
          racket/list
+         racket/set
          racket/string)
+
+(define ORIGIN (cons 0 0))
 
 (define N (cons 0 1))
 (define S (cons 0 -1))
@@ -20,14 +23,57 @@
 (define (L from)
   (R (R (R from))))
 
-(define (walk direction from)
-  (cons (+ (car from) (car direction))
-        (+ (cdr from) (cdr direction))))
+(define (turn facing direction)
+  (if (eq? (string-ref direction 0) #\R)
+      (R facing)
+      (L facing)))
 
-(define (parse-distance d)
-  (string->number (substring d 1)))
+(define (all-steps so-far legs facing)
+  (if (empty? legs) so-far
+    (let* ([leg        (first legs)]
+           [direction  (substring leg 0 1)]
+           [now-facing (turn facing direction)]
+           [distance   (string->number (substring leg 1))]
+           [steps      (for/list ([i (in-range distance)]) now-facing)])
+      (all-steps (append so-far steps) (rest legs) now-facing))))
 
-(define INPUT (file->string "01.txt"))
+(define (walk at step)
+  (cons (+ (car at) (car step))
+        (+ (cdr at) (cdr step))))
+
+(define (walk-all at steps)
+  (if (empty? steps) at
+    (walk-all (walk at (first steps)) (rest steps))))
+
+(define (distance-from-origin at)
+  (+ (abs (car at)) (abs (cdr at))))
+
+(define INPUT
+  (string-split (file->string "01.txt") ", "))
+
+(define STEPS
+  (all-steps (list) INPUT N))
+
+(define (part1 steps)
+  (distance-from-origin (walk-all ORIGIN steps)))
+
+(define answer1 (part1 STEPS))
+
+(printf "part 1: ~a ~a ~n"
+  (= 291 answer1) answer1)
+
+(define (walk-until-criss-cross visited at steps)
+  (if (set-member? visited at) at
+    (walk-until-criss-cross (set-add visited at)
+                            (walk at (first steps))
+                            (rest steps))))
+(define (part2 steps)
+  (distance-from-origin
+    (walk-until-criss-cross (set) ORIGIN steps)))
+
+(define answer2 (part2 STEPS))
+(printf "part 2: ~a ~a ~n"
+  (= 159 answer2) answer2)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -42,3 +88,18 @@
 (test-equal? "L" (L W) S)
 (test-equal? "L" (L S) E)
 (test-equal? "L" (L E) N)
+
+(test-equal? "turn L" (turn N "L14") W)
+(test-equal? "turn R" (turn N "R14") E)
+
+(test-equal? "walk" (walk ORIGIN N) (cons 0 1))
+
+(test-equal? "all-steps"
+  (all-steps (list) (list "R2" "L2" "L1" "L3") N)
+  (list E E N N W S S S))
+
+(test-equal? "walk-all"
+  (walk-all ORIGIN (list E E N N W W S))
+  (cons 0 1))
+
+
