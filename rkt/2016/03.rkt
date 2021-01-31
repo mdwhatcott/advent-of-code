b1ff7c83070c70d915382578702b73f6989282d0
Michael Whatcott <mdwhatcott@gmail.com>
2021-01-30 22:06:13 -0700

    Ready for 2016 day 3 in racket

---
+++ rkt/2016/03.rkt
@@ -0,0 +1,13 @@
+#!/usr/bin/env racket
+
+#lang racket/base
+
+(require racket/file
+         racket/list
+         racket/string)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require rackunit)
+
+(test-equal? "" 1 1)
