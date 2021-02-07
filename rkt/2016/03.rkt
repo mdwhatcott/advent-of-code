d4c8f2e963aa9abac92b186a6a059d66666b3a29
Michael Whatcott <mdwhatcott@gmail.com>
2021-02-06 19:41:11 -0700

    A few more extractions.

--- rkt/2016/03.rkt
+++ rkt/2016/03.rkt
@@ -30,17 +30,17 @@
 
 (define (parse-triangles-2 numbers)
   (if (empty? numbers) empty
-    (let ([a1 (first numbers)]  [b1 (fourth numbers)] [c1 (seventh numbers)]
-          [a2 (second numbers)] [b2 (fifth numbers)]  [c2 (eighth numbers)]
-          [a3 (third numbers)]  [b3 (sixth numbers)]  [c3 (ninth numbers)])
-      (append (list (list a1 b1 c1) (list a2 b2 c2) (list a3 b3 c3))
-        (parse-triangles-2 (list-tail numbers 9))))))
+    (let* ([a1 (first numbers)]  [b1 (fourth numbers)] [c1 (seventh numbers)]
+           [a2 (second numbers)] [b2 (fifth numbers)]  [c2 (eighth numbers)]
+           [a3 (third numbers)]  [b3 (sixth numbers)]  [c3 (ninth numbers)]
+           [t1 (list a1 b1 c1)]  [t2 (list a2 b2 c2)]  [t3 (list a3 b3 c3)])
+      (append (list t1 t2 t3) (parse-triangles-2 (list-tail numbers 9))))))
 
 (let* ([numbers (read-ints)]
-       [triangles-1 (parse-triangles-1 numbers)]
-       [triangles-2 (parse-triangles-2 numbers)]
-       [part1 (count-valid triangles-1)]
-       [part2 (count-valid triangles-2)])
+       [triangles-part-1 (parse-triangles-1 numbers)]
+       [triangles-part-2 (parse-triangles-2 numbers)]
+       [part1 (count-valid triangles-part-1)]
+       [part2 (count-valid triangles-part-2)])
   (printf "[~a] part 1: ~a ~n" (= 983 part1) part1)
   (printf "[~a] part 2: ~a ~n" (= 1836 part2) part2))
 
