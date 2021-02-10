402ae855e8de4907f436b0463d68da3eab2fbf81
Michael Whatcott <mdwhatcott@gmail.com>
2021-02-09 23:15:24 -0700

    Just starting on 2016 day 4 in racket

--- rkt/2016/04.rkt
+++ rkt/2016/04.rkt
@@ -7,6 +7,17 @@
          racket/string
          racket/match)
 
+(define (room-checksum line)
+  (second (regexp-match #px"\\[(.+)\\]" line)))
+
+(define (sector-id line)
+  (second (regexp-match #px"-(\\d+)\\[" line)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require rackunit)
+
+(define EXAMPLE1 "aaaaa-bbb-z-y-x-123[abxyz]")
+
+(test-equal? "room-checksum" (room-checksum EXAMPLE1) "abxyz")
+(test-equal? "sector-id" (sector-id EXAMPLE1) "123")
