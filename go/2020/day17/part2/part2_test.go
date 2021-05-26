2cbc6c711d1f11b262a67d437a675b441287871f
Michael Whatcott <mdwhatcott@gmail.com>
2021-05-25 23:37:09 -0600

    Copy of part1 for part2 (will modify shortly).

---
+++ go/2020/day17/part2/part2_test.go
@@ -0,0 +1,53 @@
+package part2
+
+import (
+	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestConwayFixture(t *testing.T) {
+	gunit.Run(new(ConwayFixture), t)
+}
+
+type ConwayFixture struct {
+	*gunit.Fixture
+	world World
+}
+
+func (this *ConwayFixture) Setup() {
+	this.world = make(World)
+	this.world.Set(Point(1, 0, 0), true)
+	this.world.Set(Point(2, 1, 0), true)
+	this.world.Set(Point(0, 2, 0), true)
+	this.world.Set(Point(1, 2, 0), true)
+	this.world.Set(Point(2, 2, 0), true)
+	this.AssertActive(5)
+}
+func (this *ConwayFixture) AssertActive(expected int) {
+	this.So(this.world, should.HaveLength, expected)
+}
+func (this *ConwayFixture) TestParseInitialWorld() {
+	world := ParseInitialWorld(".#.\n..#\n###")
+	this.So(world, should.Resemble, this.world)
+}
+func (this *ConwayFixture) TestNeighbors3d() {
+	unique := make(map[P]struct{})
+	for _, p := range Point(0, 0, 0).Neighbors3d() {
+		unique[p] = struct{}{}
+	}
+	this.So(unique, should.HaveLength, 26)
+}
+func (this *ConwayFixture) TestCountActiveNeighbors() {
+	neighbors := this.world.countActiveNeighbors(Point(1, 1, 0))
+	this.So(neighbors, should.Equal, 5)
+}
+func (this *ConwayFixture) TestOneCycle() {
+	this.world.Cycle()
+	this.AssertActive(11)
+}
+func (this *ConwayFixture) TestSixCycles() {
+	this.world.Boot()
+	this.AssertActive(112)
+}
