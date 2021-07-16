c7b61e796a159e66531fcf6d312465cc4d719422
Michael Whatcott <mdwhatcott@gmail.com>
2021-07-16 13:44:34 -0600

    2015 day 1 part 2 in clojure

--- clj/2015/01.clj
+++ clj/2015/01.clj
@@ -1,5 +1,23 @@
 (def input (slurp "01.txt"))
 
-(println "part 1:"
-         (- (count (filter #(= \( %) input))
-            (count (filter #(= \) %) input))))
+(defn count-char [char input]
+  (count (filter #(= char %) input)))
+
+(defn walk [input]
+  (- (count-char \( input)
+     (count-char \) input)))
+
+(println "part 1:" (walk input))
+
+;; super inefficient!
+;; (walks incrementally larger sub-
+;; strings until we arrive at -1)
+(defn walk-until [low-point input]
+  (->> (range 1 (inc (count input)))
+       (map #(subs input 0 %))
+       (map walk)
+       (take-while #(> % low-point))
+       (count)
+       (inc)))
+
+(println "part 2:" (walk-until -1 input))
