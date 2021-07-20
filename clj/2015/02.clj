903a95f49695dc02101b639839464856f7308c26
Michael Whatcott <mdwhatcott@gmail.com>
2021-07-19 23:57:15 -0600

    Parse dimensions earlier and destructure upon passing to functions.

--- clj/2015/02.clj
+++ clj/2015/02.clj
@@ -6,37 +6,35 @@
        (rest)    ; (        "2"   "3"   "4")
        (map #(Integer/parseInt %)))) ; (2 3 4)
 
-(defn paper-needed [line]
-  (let [[l w h] (dimensions line)
-        lw (* l w)
-        wh (* w h)
-        hl (* h l)
+(defn paper-needed [[length width height]]
+  (let [lw (* length width)
+        wh (* width height)
+        hl (* height length)
         slack (min lw wh hl)
         area (* 2 (+ lw wh hl))]
     (+ area slack)))
 
-(defn ribbon-needed [line]
-  (let [[l w h] (dimensions line)
-        volume (* l w h)
-        lw (* 2 (+ l w))
-        wh (* 2 (+ w h))
-        hl (* 2 (+ h l))
+(defn ribbon-needed [[length width height]]
+  (let [volume (* length width height)
+        lw (* 2 (+ length width))
+        wh (* 2 (+ width height))
+        hl (* 2 (+ height length))
         min-perimeter (min lw wh hl)]
     (+ volume min-perimeter)))
 
 (require '[clojure.test :refer :all])
 
+
 (deftest day1
+
+  (def all-dimensions (map dimensions lines))
+
   (testing "components"
-    (is (= 58 (paper-needed "2x3x4")))
-    (is (= 43 (paper-needed "1x1x10")))
-    (is (= 34 (ribbon-needed "2x3x4")))
-    (is (= 14 (ribbon-needed "1x1x10"))))
-
-  (testing "part1"
-    (is (= 1606483 (reduce + (map paper-needed lines)))))
-
-  (testing "part2"
-    (is (= 3842356 (reduce + (map ribbon-needed lines))))))
+    (is (= 58 (paper-needed '(2 3 4))))
+    (is (= 43 (paper-needed '(1 1 10))))
+    (is (= 34 (ribbon-needed '(2 3 4))))
+    (is (= 14 (ribbon-needed '(1 1 10)))))
+  (testing "part 1" (is (= 1606483 (reduce + (map paper-needed all-dimensions)))))
+  (testing "part 2" (is (= 3842356 (reduce + (map ribbon-needed all-dimensions))))))
 
 (run-tests)
