69212df1e80ab0c92849e3bec43bbedc507610f2
Michael Whatcott <mdwhatcott@gmail.com>
2021-10-23 23:08:36 -0600

    Imported new benchmarking library.

--- clj/project.clj
+++ clj/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main aoc.core
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.mdwhatcott/benchmarks "0.3.1"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
