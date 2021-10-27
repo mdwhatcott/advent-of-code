261625221d8c5920feed6e0959660c651df11082
Michael Whatcott <mdwhatcott@gmail.com>
2021-10-26 23:11:36 -0600

    Moving carts in straight lines and removing collisions.

--- clj/spec/aoc/y2018/d13_spec.clj
+++ clj/spec/aoc/y2018/d13_spec.clj
@@ -1,29 +1,48 @@
 (ns aoc.y2018.d13-spec
   (:require [speclj.core :refer :all]
             [aoc.y2018.d13 :as sut]
+            [aoc.y2018.d13 :refer [L R U D]]
             [clojure.string :as string]
             [clojure.string :refer [trim]]))
 
-(def sample-map-1 (slurp "data/2018/d13/sample-map-1.txt"))
+(def sample-tracks-1 (slurp "data/2018/d13/sample-tracks-1.txt"))
+(def sample-tracks-2 (slurp "data/2018/d13/sample-tracks-2.txt"))
+(def sample-tracks-3a (slurp "data/2018/d13/sample-tracks-3-a.txt"))
+(def sample-tracks-3b (slurp "data/2018/d13/sample-tracks-3-b.txt"))
+(def sample-tracks-3c (slurp "data/2018/d13/sample-tracks-3-c.txt"))
+(def sample-tracks-3d (slurp "data/2018/d13/sample-tracks-3-d.txt"))
+(def sample-tracks-3e (slurp "data/2018/d13/sample-tracks-3-e.txt"))
+(def sample-tracks-4 (slurp "data/2018/d13/sample-tracks-4.txt"))
 
 (describe "2018 Day 13"
   (context "Part 1"
     (it "parses the tracks"
-      (->> (sut/parse-map sample-map-1)
+      (->> (sut/parse-tracks sample-tracks-1)
            (should= {[0 0] "/",, [1 0] "-", [2 0] "-", [3 0] "-", [4 0] "-", [5 0] "\\",
                      [0 1] "|",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, [5 1] "|",
                      [0 2] "|",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, [5 2] "|",
                      [0 3] "\\", [1 3] "-", [2 3] "-", [3 3] "-", [4 3] "-", [5 3] "/"})))
 
     (it "finds the starting position of the carts"
-      (->> (sut/find-carts sample-map-1)
+      (->> (sut/find-carts sample-tracks-1)
            ; := (current location)
            ; :> (current direction)
            ; :+ (next intersection turn)
-           (should= [{:= [2 0] :> ">" :+ "<"}
-                     {:= [5 1] :> "v" :+ "<"}
-                     {:= [0 2] :> "^" :+ "<"}
-                     {:= [3 3] :> "<" :+ "<"}])))
+           (should= [{:= [2 0] :> R :+ L}
+                     {:= [5 1] :> D :+ L}
+                     {:= [0 2] :> U :+ L}
+                     {:= [3 3] :> L :+ L}])))
+
+    (it "moves all carts along the track"
+      (let [world    (sut/parse-initial sample-tracks-3a)
+            expected (sut/parse-initial sample-tracks-3c)
+            tick1    (sut/tick world)]
+        (->> tick1 (should= expected))))
+
+    (it "removes colliding carts from the world"
+      (let [world  (sut/parse-initial sample-tracks-3a)
+            result (sut/tick (sut/tick world))]
+        (should (empty? (:carts result)))))
     )
 
   (context "Part 2"
--- clj/src/aoc/y2018/d13.clj
+++ clj/src/aoc/y2018/d13.clj
@@ -2,6 +2,17 @@
   (:require [clojure.string :as string]
             [clojure.pprint :as pprint]))
 
+(def R [1 0])
+(def L [-1 0])
+(def D [0 1])
+(def U [0 -1])
+
+(def cart->d
+  {"<" L
+   ">" R
+   "v" D
+   "^" U})
+
 (defn widest-line [lines]
   (apply max (map count lines)))
 
@@ -13,7 +24,7 @@
         :when (not= char " ")]
     [[x y] char]))
 
-(defn parse-map [raw-map]
+(defn parse-tracks [raw-map]
   (as-> raw-map $
         (string/replace $ "v" "|")
         (string/replace $ "^" "|")
@@ -30,8 +41,36 @@
               char (str (get line x ""))]
         :when (#{"<" ">" "v" "^"} char)]
     {:= [x y]
-     :> char
-     :+ "<"}))
+     :> (get cart->d char)
+     :+ L}))
 
 (defn find-carts [raw-map]
   (parse-carts (string/split-lines raw-map)))
+
+(defn parse-initial [raw-map]
+  {:tracks (parse-tracks raw-map)
+   :carts  (find-carts raw-map)})
+
+(defn sorting-order [cart]
+  (let [[x y] (:= cart)] [y x]))
+
+(defn move [cart tracks]
+  (let [[x1 y1] (:= cart)
+        [dx dy] (:> cart)
+        target [(+ x1 dx)
+                (+ y1 dy)]]
+    (assoc cart := target)))
+
+(defn tick [world]
+  (loop [carts  (:carts world)
+         carts2 {}]
+    (let [carts (sort-by sorting-order carts)]
+      (if (empty? carts)
+        (assoc world :carts (vals carts2))
+        (let [cart2  (move (first carts) (:tracks world))
+              target (:= cart2)
+              collision? (contains? carts2 target)]
+          (if collision?
+            (recur (rest carts) (dissoc carts2 target))
+            (recur (rest carts) (assoc carts2 target cart2)))))))
+  )
