b753a2366580b43a5e6919fd3ae7eae4333ec961
Michael Whatcott <mdwhatcott@gmail.com>
2021-11-08 09:22:37 -0700

    Still working through 2018 day 13 in Clojure.

--- clj/spec/aoc/y2018/d13_spec.clj
+++ clj/spec/aoc/y2018/d13_spec.clj
@@ -1,19 +1,14 @@
 (ns aoc.y2018.d13-spec
   (:require [speclj.core :refer :all]
             [aoc.y2018.d13 :as sut]
-            [aoc.y2018.d13 :refer [L R U D]]
-            [clojure.string :as string]
-            [clojure.string :refer [trim]]
-            [clojure.pprint :as pprint]))
+            [aoc.y2018.d13 :refer [L R U D S]]
+            [clojure.string :refer [trim]]))
 
 (def sample-tracks-1 (slurp "data/2018/d13/sample-tracks-1.txt"))
-(def sample-tracks-2 (slurp "data/2018/d13/sample-tracks-2.txt"))
 (def sample-tracks-3a (slurp "data/2018/d13/sample-tracks-3-a.txt"))
-(def sample-tracks-3b (slurp "data/2018/d13/sample-tracks-3-b.txt"))
 (def sample-tracks-3c (slurp "data/2018/d13/sample-tracks-3-c.txt"))
-(def sample-tracks-3d (slurp "data/2018/d13/sample-tracks-3-d.txt"))
-(def sample-tracks-3e (slurp "data/2018/d13/sample-tracks-3-e.txt"))
 (def sample-tracks-4 (slurp "data/2018/d13/sample-tracks-4.txt"))
+(def real-tracks (slurp "data/2018/d13.txt"))
 
 (describe "2018 Day 13"
   (context "Part 1"
@@ -46,54 +41,44 @@
             result (sut/tick (sut/tick world))]
         (should (empty? (:carts result)))))
 
-    (context "turning corners"
-      (it "R -> U"
-        (let [cart {:= [1 1] :> R} tracks {[2 1] "\\"}]
-          (should= U (:> (sut/move cart tracks)))))
+    (context "turning corners and going through intersections"
+      (with tracks {[1 1] "-" [2 2] "|" [3 3] "/" [4 4] "\\" [5 5] "+"})
 
-      (it "L -> D"
-        (let [cart {:= [1 1] :> L} tracks {[0 1] "\\"}]
-          (should= D (:> (sut/move cart tracks)))))
+      (it "R + \\ = D" (-> {:= [3 4] :> R} (sut/move @tracks) :> (should= D)))
+      (it "L + \\ = U" (-> {:= [5 4] :> L} (sut/move @tracks) :> (should= U)))
+      (it "U + \\ = L" (-> {:= [4 5] :> U} (sut/move @tracks) :> (should= L)))
+      (it "D + \\ = R" (-> {:= [4 3] :> D} (sut/move @tracks) :> (should= R)))
 
-      (it "D -> R"
-        (let [cart {:= [1 1] :> D} tracks {[1 2] "\\"}]
-          (should= R (:> (sut/move cart tracks)))))
+      (it "R + / = U" (-> {:= [2 3] :> R} (sut/move @tracks) :> (should= U)))
+      (it "L + / = D" (-> {:= [4 3] :> L} (sut/move @tracks) :> (should= D)))
+      (it "U + / = R" (-> {:= [3 4] :> U} (sut/move @tracks) :> (should= R)))
+      (it "D + / = L" (-> {:= [3 2] :> D} (sut/move @tracks) :> (should= L)))
 
-      (it "U -> L"
-        (let [cart {:= [1 1] :> U} tracks {[1 0] "\\"}]
-          (should= L (:> (sut/move cart tracks)))))
+      (it "L@+ U = L" (-> {:= [5 6] :> U :+ L} (sut/move @tracks) (should= {:= [5 5] :> L :+ S})))
+      (it "L@+ L = D" (-> {:= [6 5] :> L :+ L} (sut/move @tracks) (should= {:= [5 5] :> D :+ S})))
+      (it "L@+ D = R" (-> {:= [5 4] :> D :+ L} (sut/move @tracks) (should= {:= [5 5] :> R :+ S})))
+      (it "L@+ R = U" (-> {:= [4 5] :> R :+ L} (sut/move @tracks) (should= {:= [5 5] :> U :+ S})))
 
-      (it "U -> R"
-        (let [cart   {:= [1 1] :> U}
-              tracks {[1 0] "/"}]
-          (should= R (:> (sut/move cart tracks)))))
+      (it "S@+ R = R" (-> {:= [4 5] :> R :+ S} (sut/move @tracks) (should= {:= [5 5] :> R :+ R})))
+      (it "S@+ L = L" (-> {:= [6 5] :> L :+ S} (sut/move @tracks) (should= {:= [5 5] :> L :+ R})))
+      (it "S@+ U = U" (-> {:= [5 6] :> U :+ S} (sut/move @tracks) (should= {:= [5 5] :> U :+ R})))
+      (it "S@+ D = D" (-> {:= [5 4] :> D :+ S} (sut/move @tracks) (should= {:= [5 5] :> D :+ R})))
 
-      (it "D -> L"
-        (let [cart   {:= [1 1] :> D}
-              tracks {[1 2] "/"}]
-          (should= L (:> (sut/move cart tracks)))))
-
-      (it "L -> U"
-        (let [cart   {:= [1 1] :> L}
-              tracks {[0 1] "/"}]
-          (should= U (:> (sut/move cart tracks)))))
-
-      (it "R -> D"
-        (let [cart   {:= [1 1] :> R}
-              tracks {[2 1] "/"}]
-          (should= D (:> (sut/move cart tracks)))))
+      (it "R@+ U = R" (-> {:= [5 6] :> U :+ R} (sut/move @tracks) (should= {:= [5 5] :> R :+ L})))
+      (it "R@+ R = D" (-> {:= [4 5] :> R :+ R} (sut/move @tracks) (should= {:= [5 5] :> D :+ L})))
+      (it "R@+ D = L" (-> {:= [5 4] :> D :+ R} (sut/move @tracks) (should= {:= [5 5] :> L :+ L})))
+      (it "R@+ L = U" (-> {:= [6 5] :> L :+ R} (sut/move @tracks) (should= {:= [5 5] :> U :+ L})))
       )
 
-    (context "intersections")
+    (it "registers-collisions"
+      (let [world     (sut/parse-initial sample-tracks-4)
+            collision (sut/until-first-collision world)]
+        (should= [7 3] collision)))
 
-    #_(it "registers-collisions"
-        (let [world     (sut/parse-initial sample-tracks-4)
-              collision (as-> (iterate sut/tick world) $
-                              (take 2 $)
-                              (last $)
-                              (do (println $) $)
-                              (:collisions $))]
-          (should= [7 3] collision)))
+    (it "solves with real data"
+      (let [world     (sut/parse-initial real-tracks)
+            collision (sut/until-first-collision world)]
+        (should= [14 42] collision)))
     )
 
   (context "Part 2"
--- clj/src/aoc/y2018/d13.clj
+++ clj/src/aoc/y2018/d13.clj
@@ -7,6 +7,8 @@
 (def D [0 1])
 (def U [0 -1])
 
+(def S [0 0])
+
 (def cart->d
   {"<" L
    ">" R
@@ -60,31 +62,75 @@
     [(+ x1 dx)
      (+ y1 dy)]))
 
+(defn turn [turn-direction current-direction]
+  (condp = turn-direction
+    S current-direction
+    L (condp = current-direction U L, L D, D R, R U)
+    R (condp = current-direction R D, L U, D L, U R)))
+
 (defn orient [cart track]
   (let [direction (:> cart)]
     (case track
-      "|" direction
-      "-" direction
-      "/", (condp = direction R D, L U, U R, D L)
-      "\\" (condp = direction R U, L D, U L, D R))))
+      "|", direction
+      "-", direction
+      "+", (turn (:+ cart) direction)
+      "/", (condp = direction R U, L D, U R, D L)
+      "\\" (condp = direction R D, L U, U L, D R))))
+
+(defn intersection [cart track]
+  (if-not (= track "+")
+    (:+ cart)
+    (condp = (:+ cart) L S, S R, R L)))
 
 (defn move [cart tracks]
-  (let [target    (advance cart)
-        track     (get tracks target)
-        direction (orient cart track)]
+  (let [target         (advance cart)
+        track          (get tracks target)
+        direction      (orient cart track)
+        turn-direction (intersection cart track)]
     (assoc cart := target
-                :> direction)))
+                :> direction
+                :+ turn-direction)))
 
 (defn tick [world]
-  (loop [carts  (:carts world)
-         carts2 {}]
+  (loop [carts      (:carts world)
+         carts2     {}
+         positions  #{}
+         collisions []]
+    (println "carts: " carts)
+    (println "carts2:" carts2)
     (let [carts (sort-by sorting-order carts)]
       (if (empty? carts)
-        (assoc world :carts (vals carts2))
-        (let [cart2  (move (first carts) (:tracks world))
-              target (:= cart2)
-              collision? (contains? carts2 target)]
+        (assoc world :carts (vals carts2) :collisions collisions)
+        (let [moved      (move (first carts) (:tracks world))
+              target     (:= moved)
+              collision? (contains? positions target)]
           (if collision?
-            (recur (rest carts) (dissoc carts2 target))
-            (recur (rest carts) (assoc carts2 target cart2)))))))
+
+            (recur (rest carts)
+                   (dissoc carts2 target)
+                   (disj positions target)
+                   (conj collisions target))
+
+            (recur (rest carts)
+                   (assoc carts2 target moved)
+                   (conj positions target)
+                   collisions))))))
   )
+
+(defn until-first-collision [world]
+  (loop [world1 world]
+    (let [world2     (tick world1)
+          collisions (:collisions world2)]
+      (if (seq collisions)
+        (first collisions)
+        (do
+          (Thread/sleep 100)
+          (recur world2)))))
+
+
+  #_(as-> (iterate tick world) $
+          (drop-while #(or (empty? (:collisions %))
+                           (empty? (:carts %))) $)
+          (first $)
+          (:collisions $)
+          (first $)))
