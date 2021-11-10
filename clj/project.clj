793d66355081d85f721d8a6e58461c8b82c976c9
Michael Whatcott <mdwhatcott@gmail.com>
2021-11-09 22:02:11 -0700

    Script to generate puzzle skeletons, ready to implement.

--- clj/project.clj
+++ clj/project.clj
@@ -2,10 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :main aoc.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.mdwhatcott/benchmarks "0.3.1"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
-  :test-paths ["spec"])
+  :test-paths ["spec"]
+  :aliases {"aoc" ["run" "-m" "aoc.tools.generator"] #_"ie. lein aoc 2021 1"}
+  )
---
+++ clj/src/aoc/tools/generator.clj
@@ -0,0 +1,32 @@
+(ns aoc.tools.generator
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn format-namespace [s]
+  (let [n (Integer/parseInt s)]
+    (if (< n 10) (str "0" s) s)))
+
+(def prod-template (slurp "src/aoc/tools/generator/prod-template.txt"))
+(def spec-template (slurp "src/aoc/tools/generator/spec-template.txt"))
+
+(defn replacer [content [key value]]
+  (str/replace content (format "!%s!" key) value))
+
+(defn template [raw data]
+  (reduce replacer raw (seq data)))
+
+(defn write-file [path content]
+  (println "Creating file:" path)
+  (io/make-parents path)
+  (spit path content))
+
+(defn -main [year day]
+  (let [namespace    (format-namespace day)
+        data         {"Y" year "NS" namespace "D" day}
+        spec-path    (format "./spec/aoc/y%s/d%s_spec.clj" year namespace)
+        prod-path    (format "./src/aoc/y%s/d%s.clj" year namespace)
+        spec-content (template spec-template data)
+        prod-content (template prod-template data)]
+    (println (format "Initializing %s Day %s..." year day))
+    (write-file spec-path spec-content)
+    (write-file prod-path prod-content)))
