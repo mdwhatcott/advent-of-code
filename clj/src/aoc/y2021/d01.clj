49851c054a3b80bd77ee7523557da0edb4595893
Michael Whatcott <mdwhatcott@gmail.com>
2021-11-30 22:36:21 -0700

    More direct approach with (filter (apply < ...)).

--- clj/src/aoc/y2021/d01.clj
+++ clj/src/aoc/y2021/d01.clj
@@ -1,14 +1,11 @@
 (ns aoc.y2021.d01)
 
 (defn count-increases [data]
-  (as-> data $
-        (partition 2 1 $)
-        (map #(- (first %1) (last %1)) $)
-        (filter neg? $)
-        (count $)))
+  (->> (partition 2 1 data)
+       (filter #(apply < %))
+       count))
 
 (defn count-window-increases [window data]
-  (as-> data $
-        (partition window 1 $)
-        (map #(apply + %) $)
-        (count-increases $)))
+  (->> (partition window 1 data)
+       (map #(apply + %))
+       count-increases))
