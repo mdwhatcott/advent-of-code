7d9136e48f9b3f2ef16c556fb115a045a77a38bc
Michael Whatcott <mdwhatcott@gmail.com>
2021-12-05 00:21:04 -0700

    Solved 2021 day 4 part 1 in Clojure

---
+++ clj/spec/aoc/y2021/d04_spec.clj
@@ -0,0 +1,111 @@
+(ns aoc.y2021.d04-spec
+  (:require [speclj.core :refer :all]
+            [aoc.y2021.d04 :as sut]
+            [aoc.data :as data]))
+
+(def real-data
+  (data/read-lines 2021 4))
+
+(def sample-data
+  ["7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"
+   ""
+   "22 13 17 11  0"
+   " 8  2 23  4 24"
+   "21  9 14 16  7"
+   " 6 10  3 18  5"
+   " 1 12 20 15 19"
+   ""
+   " 3 15  0  2 22"
+   " 9 18 13 17  5"
+   "19  8  7 25 23"
+   "20 11 10 24  4"
+   "14 21 16 12  6"
+   ""
+   "14 21 17 24  4"
+   "10 16 15  9 19"
+   "18  8 23 26 20"
+   "22 11 13  6  5"
+   " 2  0 12  3  7"])
+
+(def call-order
+  [7 4 9 5 11 17 23 2 0 14 21 24 10 16 13 6 15 25 12 22 18 20 8 19 3 26 1])
+
+(def raw-boards
+  [["22 13 17 11  0"
+    " 8  2 23  4 24"
+    "21  9 14 16  7"
+    " 6 10  3 18  5"
+    " 1 12 20 15 19"]
+   [" 3 15  0  2 22"
+    " 9 18 13 17  5"
+    "19  8  7 25 23"
+    "20 11 10 24  4"
+    "14 21 16 12  6"]
+   ["14 21 17 24  4"
+    "10 16 15  9 19"
+    "18  8 23 26 20"
+    "22 11 13  6  5"
+    " 2  0 12  3  7"]])
+
+(describe "2021 Day 4"
+  (context "Part 1"
+    (it "parses the input"
+      (should= {:call-order call-order
+                :boards     raw-boards}
+               (sut/parse-input sample-data)))
+
+    (context "Bingo Board"
+      (it "converts raw input to board structure"
+        (should= {:score      0
+                  :checksum   0
+                  :call-order call-order
+                  :called     []
+                  :marked     #{}
+                  :unmarked   #{22 13 17 11 0
+                                8, 2, 23 4, 24
+                                21 9, 14 16 7
+                                6, 10 3, 18 5
+                                1, 12 20 15 19}
+                  :wins       [#{22 13 17 11 0,}
+                               #{8, 2, 23 4, 24}
+                               #{21 9, 14 16 7,}
+                               #{6, 10 3, 18 5,}
+                               #{1, 12 20 15 19}
+                               #{22 8, 21 6, 1,}
+                               #{13 2, 9, 10 12}
+                               #{17 23 14 3, 20}
+                               #{11 4, 16 18 15}
+                               #{0, 24 7, 5, 19}]}
+                 (sut/prepare-board call-order (first raw-boards))))
+
+      (it "tracks a called number"
+        (let [board (sut/prepare-board call-order (first raw-boards))
+              board (sut/call-next-number board)]
+          (should-contain 7 (:marked board))
+          (should-not-contain 7 (:unmarked board))
+          (should-not-contain 7 (:call-order board))
+          (should= [7] (:called board))))
+
+      (it "plays a game"
+        (let [board (sut/prepare-board call-order (last raw-boards))
+              board (sut/play board)]
+          (should= 24 (peek (:called board)))
+          (should= 188 (:score board))
+          (should= 4512 (:checksum board)))))
+
+    (it "solves with sample data"
+      (should= 4512 (sut/part1 sample-data)))
+
+    (it "solves with real data"
+      (should= 29440 (sut/part1 real-data)))
+
+    )
+
+  #_(context "Part 2"
+      #_(it "solves with sample data"
+          (sut/part2 sample-data))
+
+      #_(it "solves with real data"
+          (sut/part2 real-data))
+      )
+  )
---
+++ clj/src/aoc/y2021/d04.clj
@@ -0,0 +1,83 @@
+(ns aoc.y2021.d04
+  (:require [aoc.data :as data]
+            [clojure.string :as string]
+            [clojure.pprint :as pprint]
+            [clojure.set :as set]))
+
+(defn parse-input [lines]
+  {:call-order (as-> (first lines) $
+                     (string/split $ #",")
+                     (map data/str->int $))
+   :boards     (as-> (rest lines) $
+                     (partition 6 $)
+                     (map #(drop 1 %) $))})
+
+(defn- board-numbers [lines]
+  (as-> lines $
+        (map #(str % " ") $)
+        (apply str $)
+        (string/split $ #"\s")
+        (remove empty? $)
+        (map data/str->int $)))
+
+(defn row-sets [numbers]
+  (map set (partition 5 numbers)))
+
+(defn nth-nums [col rows]
+  (map #(nth (seq %) col) rows))
+
+(defn col-sets [numbers]
+  (let [rows (partition 5 numbers)]
+    (as-> (range (count rows)) $
+          (map #(nth-nums % rows) $)
+          (map set $))))
+
+(defn prepare-board [call-order lines]
+  (let [numbers (board-numbers lines)]
+    {:score      0
+     :checksum   0
+     :call-order call-order
+     :called     []
+     :marked     #{}
+     :unmarked   (set numbers)
+     :wins       (concat (row-sets numbers)
+                         (col-sets numbers))}))
+
+(defn call-next-number [board]
+  (let [num   (first (:call-order board))
+        board (-> board
+                  (update :call-order rest)
+                  (update :called conj num))]
+    (if-not (contains? (:unmarked board) num)
+      board
+      (-> board
+          (update :marked conj num)
+          (update :unmarked disj num)))))
+
+(defn- bingo? [{:keys [marked wins]}]
+  (some? (first (filter #(set/subset? % marked) wins))))
+
+(defn score [board]
+  (let [board (call-next-number board)]
+    (if-not (bingo? board)
+      board
+      (let [score  (apply + (:unmarked board))
+            called (peek (:called board))]
+        (assoc board :score score
+                     :checksum (* score called))))))
+
+(defn play [board]
+  (as-> (iterate score board) $
+        (drop-while #(and (some? (:call-order %))
+                          (zero? (:score %))) $)
+        (first $)))
+
+(defn part1 [data]
+  (let [parsed  (parse-input data)
+        boards  (map #(prepare-board (:call-order parsed) %)
+                     (:boards parsed))
+        played  (map play boards)
+        winners (filter #(pos? (:score %)) played)]
+    (:checksum (first (sort-by #(count (:called %)) winners)))))
+
+(defn part2 [data])
