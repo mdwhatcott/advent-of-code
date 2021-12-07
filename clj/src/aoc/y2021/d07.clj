5241ebcf8a7863c1446fbec9da641b9be9c8a71a
Michael Whatcott <mdwhatcott@gmail.com>
2021-12-07 00:35:37 -0700

    Ready to post.

--- clj/src/aoc/y2021/d07.clj
+++ clj/src/aoc/y2021/d07.clj
@@ -3,18 +3,13 @@
 (defn abs [n] (if (neg? n) (- n) n))
 
 (defn part1-fuel-cost [distance] distance)
+(defn part2-fuel-cost [distance] (apply + (range 1 (inc distance))))
 
 (defn lowest-fuel-cost [data cost]
-  (let [lowest  (apply min data)
-        highest (apply max data)]
-    (apply min (for [i (range lowest (inc highest))]
+  (let [lo (apply min data)
+        hi (apply max data)]
+    (apply min (for [i (range lo (inc hi))]
                  (apply + (map #(cost (abs (- i %))) data))))))
 
-(defn part1 [data]
-  (lowest-fuel-cost data part1-fuel-cost))
-
-(defn part2-fuel-cost [distance]
-  (apply + (range 1 (inc distance))))
-
-(defn part2 [data]
-  (lowest-fuel-cost data (memoize part2-fuel-cost)))
+(defn part1 [data] (lowest-fuel-cost data part1-fuel-cost))
+(defn part2 [data] (lowest-fuel-cost data (memoize part2-fuel-cost)))
