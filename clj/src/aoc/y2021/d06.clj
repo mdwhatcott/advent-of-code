f1c4780e0881340fc179769306f13acc17daf124
Michael Whatcott <mdwhatcott@gmail.com>
2021-12-07 21:59:03 -0700

    Fred Overflow is amazing.

--- clj/src/aoc/y2021/d06.clj
+++ clj/src/aoc/y2021/d06.clj
@@ -1,16 +1,10 @@
 (ns aoc.y2021.d06)
 
 ; Inspiration:
-; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnj18bu/
-; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfaisu/
-; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfd2w5/
-(defn swim [fishes]
-  (for [i (range 9)]
-    (case i
-      6 (+ (nth fishes 0)                                   ; parents
-           (nth fishes (inc i)))
-      8 (nth fishes 0)                                      ; guppies
-      (nth fishes (inc i)))))
+; Fred Overflow - https://www.youtube.com/watch?v=umHxPjNXD6Y&t=82
+(defn swim
+  [[_0 _1 _2 _3 _4 _5,,, _6,,,, _7 _8]]                     ; destructure the input sequence
+  ,[_1 _2 _3 _4 _5 _6 (+ _7 _0) _8 _0])                     ; manually shift to create output sequence
 
 (defn part1 [data days]
   (as-> (frequencies data) $
@@ -23,6 +17,18 @@
   (part1 data 256))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Inspiration:
+; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnj18bu/
+; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfaisu/
+; https://www.reddit.com/r/adventofcode/comments/r9z49j/comment/hnfd2w5/
+(defn swim--most-general [fishes]
+  (for [i (range 9)]
+    (case i
+      6 (+ (nth fishes 0)                                   ; parents
+           (nth fishes (inc i)))
+      8 (nth fishes 0)                                      ; guppies
+      (nth fishes (inc i)))))
 
 ; WARNING: exponential growth makes this function unbearably slow at days >= 200
 ; Only included here for completeness.
