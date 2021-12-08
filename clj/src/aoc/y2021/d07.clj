48769124834f855222469c59c2dfb1512cc0de16
Michael Whatcott <mdwhatcott@gmail.com>
2021-12-07 22:46:23 -0700

    Any day you find you don't need memoize after all is a good day.

--- clj/src/aoc/y2021/d07.clj
+++ clj/src/aoc/y2021/d07.clj
@@ -9,7 +9,12 @@
                  (apply + (map #(cost (abs (- i %))) data))))))
 
 (defn part1-fuel-cost [distance] distance)
-(defn part2-fuel-cost [distance] (apply + (range 1 (inc distance))))
+(defn part2-fuel-cost [distance] (/ (* distance (inc distance)) 2))
 
 (defn part1 [data] (lowest-fuel-cost data part1-fuel-cost))
-(defn part2 [data] (lowest-fuel-cost data (memoize part2-fuel-cost)))
+(defn part2 [data] (lowest-fuel-cost data part2-fuel-cost))
+
+; Triangle number formula in part2-fuel-cost courtesy of KnavesRadiant on Reddit:
+; https://www.reddit.com/r/adventofcode/comments/rar7ty/2021_day_7_solutions/hnlrphb/
+; https://en.wikipedia.org/wiki/Triangular_number
+; Formula: n(n+1) / 2
