e7ddb89503337cca45f87cce1950cab7659c1397
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-09-19 15:50:00 -0600

    Solved 2016 day 13 part 2 in Clojure, with visualization of path.

--- clj-lein/spec/aoc/y2016/d13_spec.clj
+++ clj-lein/spec/aoc/y2016/d13_spec.clj
@@ -12,23 +12,7 @@
        count))
 
 (defn hallway? [seed [x y]]
-  (and (not (neg? x))
-       (not (neg? y))
-       (->> seed
-            (+ (* x x) (* 3 x) (* 2 x y) y (* y y))
-            bits
-            even?)))
-
-(defn bfs [origin target graph]
-  (loop [seen #{} frontier [[origin 0]]]
-    (let [[at dist] (peek frontier)
-          frontier (pop frontier)]
-      (if (= at target)
-        dist
-        (let [neighbors (remove seen (graph at))
-              steps     (for [n neighbors] [n (inc dist)])]
-          (recur (conj seen at)
-                 (vec (concat steps frontier))))))))
+  (->> seed (+ (* x x) (* 3 x) (* 2 x y) y (* y y)) bits even?))
 
 (defn cardinal-neighbors [[x y]]
   [[(dec x) y]
@@ -39,24 +23,51 @@
 (defn maze-neighbors [seed point]
   (filter (partial hallway? seed) (cardinal-neighbors point)))
 
+(defn bfs-step [graph {:keys [explored frontier]}]
+  (let [current   (peek frontier)
+        frontier  (pop frontier)
+        neighbors (remove #(contains? explored %) (graph current))]
+    {:frontier (reduce conj (vec neighbors) frontier)
+     :explored (reduce #(assoc %1 %2 current) explored neighbors)}))
+
+(defn trace [target explored]
+  (loop [path [target]]
+    (let [step (explored (peek path))]
+      (if (nil? step)
+        (reverse path)
+        (recur (conj path step))))))
+
+(defn emit [seed maxX maxY explored]
+  (newline)
+  (doseq [y (range maxY)
+          x (range maxX)]
+    (when (zero? x) (newline))
+    (if-not (hallway? seed [x y])
+      (print "#")
+      (if (contains? explored [x y])
+        (print "•")
+        (print " "))))
+  (newline))
+
+(defn bfs [origin target graph emit]
+  (loop [state {:frontier [origin] :explored {origin nil}}]
+    (let [explored (:explored state)]
+      (if (contains? explored target)
+        (let [path (trace target explored)]
+          (emit (set path))
+          path)
+        (do
+          #_(emit explored)
+          (recur (bfs-step graph state)))))))
+
 (describe "2016 Day 13"
-  (it "Part 1"
-    (should= 11 (bfs [1 1] [7 4] (partial maze-neighbors 10)))
-    (should= 96 (bfs [1 1] [31 39] (partial maze-neighbors 1358))))
-
-  (it "maze neighbors"
-    (should= (cardinal-neighbors [7 1]) (maze-neighbors 10 [7 1]))
-    (should= [] (maze-neighbors 10 [5 3]))
-    (should= [[0 1] [1 2]] (maze-neighbors 10 [1 1])))
-
-  (it "breadth-first search"
-    (should= 0 (bfs :a :a {:a []}))
-    (should= 1 (bfs :a :b {:a [:b]}))
-    (should= 2 (bfs :a :c {:a [:b] :b [:c]}))
-    (should= 9 (bfs [1 1] [7 4] cardinal-neighbors)))
-
-  (it "cardinal neighbors"
-    (should= [[0 1] [2 1] [1 0] [1 2]] (cardinal-neighbors [1 1])))
+  (it "counts '1' bits"
+    (should= 0 (bits 0)) #_"0000"
+    (should= 1 (bits 1)) #_"0001"
+    (should= 1 (bits 2)) #_"0010"
+    (should= 1 (bits 4)) #_"0100"
+    (should= 2 (bits 3)) #_"0011"
+    (should= 8 (bits 255))))
 
   (it "walls vs halls"
     (should= false (hallway? 10 [0 -1]))
@@ -66,10 +77,79 @@
     (should= false (hallway? 10 [9 6]))
     (should= false (hallway? 10 [1 0])))
 
-  (it "counting '1' bits"
-    (should= 0 (bits 0)) #_"0000"
-    (should= 1 (bits 1)) #_"0001"
-    (should= 1 (bits 2)) #_"0010"
-    (should= 1 (bits 4)) #_"0100"
-    (should= 2 (bits 3)) #_"0011"
-    (should= 8 (bits 255))))
+  (it "cardinal neighbors"
+    (should= [[0 1] [2 1] [1 0] [1 2]] (cardinal-neighbors [1 1])))
+
+  (it "breadth-first search"
+    (let [expected {:frontier [:b] :explored {:a nil :b :a}}
+          input    {:frontier [:a] :explored {:a nil}}]
+      (should= expected (bfs-step {:a [:b]} input)))
+
+    (let [expected {:explored {:a nil :b :a} :frontier []}
+          input    {:explored {:a nil :b :a} :frontier [:b]}]
+      (should= expected (bfs-step {:a [:b] :b [:a]} input)))
+
+    (should= [:a :b :c] (trace :c {:c :b :b :a :a nil}))
+    (should= [:a :b :c] (bfs :a :c {:a [:b] :b [:c]} (fn [_])))
+    (should= 9 (dec (count (bfs [1 1] [7 4] cardinal-neighbors (fn [_]))))))
+
+  (it "maze neighbors"
+    (should= [] (maze-neighbors 10 [5 3]))
+    (should= (cardinal-neighbors [3 2]) (maze-neighbors 10 [3 2]))
+    (should= [[0 1] [1 2]] (maze-neighbors 10 [1 1])))
+
+  (it "part 1"
+    (should= 11 (dec (count (bfs [1 1] [7 4] (partial maze-neighbors 10) (partial emit 10 10 7)))))
+    (should= 96 (dec (count (bfs [1 1] [31 39] (partial maze-neighbors 1358) (partial emit 1358 50 50)))))))
+
+; Output of emit:
+;  ##  # # ## #    ##   #  # #### ####   #  #  #  #
+;  •##  ## ## #  #   ### #     #     ## ######    #
+; #•••# ##  ######## # #####   #  ## ## #     ## ###
+;  ##••••••••••• ###     ##### #####    #  ##  # ###
+; ##### ## ####•  # ##    #  # #  # ## #### ##    ##
+;  ## #  # ## #•# ######  # ##  # ####  # #######  #
+;    ###      #••#  #  #### ###  #  # # ##  #
+; ## ####  ### #•## # #   #  # # ## ##      #  ####
+;     ###### ###• ###  ## #  ###  ## #  ## ####   #
+; # #  ##  # ••••  ### #  ###      # #####  # #   #
+; #        ##•## #     #    # ## # ###  # # ####  ##
+;  ###### # #•##   ### ##   ## #  #  ## ##   #######
+; #    ##  ##•# ###  # ### # ## #  #  ## #     #  ##
+; #  # ### #••#   # ##   #  # #  #       ##  # ##  #
+; ####  #  #•###### ##### #  ###  ## ### ####   #
+; #     ## #•##  #   ## # ## ####  # ###    #   #  #
+; # ##   #  •••• #      #     # ###   ######## #####
+; # ### #######•######## ## # ### ##   ##  # # ##  #
+; #  ## #   ###•##•••# ## #  #     ##    # ##      #
+;  # ## #     #••••#••# ## # #### # # ##  # ##### ##
+; ##   ### ## # ### #•## ###   ##  ## # #      ## #
+;    #  ## #  ##  ## • #     #  ## #  #  ##  # ## #
+; #####    #   ### # • ## ##### #  #  ## #####  ###
+;    # ## ### #  # ##•# ###  #  ## #####          #
+; #  ####  ##  # # ##••#•••••#   # #  ##### ## ## ##
+; #    # #   #  ##  ##•••###•## ##  #  ## # ## #   #
+; ###  ## ##  # ### # #### #•## ### #     #    # # #
+; # ##  #######  #  #     ##•••• #  ###### ##  #  #
+;    ###  #      #  ### # # ###••#     # ## ### #  #
+; # #  ## #  ######## ### ### ##•##  # #  ### #  # #
+; #  #   ####    #       #   # #•##### # #   ###
+;  #   #    #  # # ##  # ###  ##•##   ## ### #### ##
+; # ############ # #####    # # ••••# ##      # # #
+; # #  ##  #    ##  #    ## # #  ##•#    ## # ##  #
+; # #    # ## # # # # ### #  ######•# ### # ## #  #
+;   ## #  # # # ##  ##  ## #     #••### ##   # ### #
+;  # ## #   # ## #   # # ######  #•#  ## # # # # ###
+;   # #  ###   # ## ## ##  ## #  #•••  # #  ##     #
+; #  ### # # # #### ##  #     ##  ##•• #  # ####
+; ## ###  ##  #   #     #  ######••##•####   ######
+;     # # # #  ## # ## #####  # ##•##•## #    #   #
+; ### ##  #  # #  ## # ##  #  ####•##•  ##### #   #
+;    # # ###   #   ##      ###  ##••••# ##  ####  #
+;  # # # #### ### # ##### #   #    ## #       #### #
+; ## #    ###  ##      ##  ##   ### # # ##  # ## ###
+;   ### #  # #   ##  # ### # ## # ##  ## ###   #   #
+; # # # #  ## ## #####  #  ## # ## #   # # #   ##  #
+; # ##  ##  #### ##     ##  ##   #### ## # ## # # #
+; ## #   ###  #     ##   ### #     ## ## # ##  ##  #
+;  #### #   # #  ## ### #    ##  #      ##  ## # #
