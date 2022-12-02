b02f9a49220d2dac620f0bb58c4b32ce0dce0382
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2022-12-01 21:46:13 -0700

    Solved 2019 day 16 part 1 in Go

--- go/2019/day16/main.go
+++ go/2019/day16/main.go
@@ -1,9 +1,79 @@
 package advent
 
-func Part1() interface{} {
-	return nil
+import (
+	"fmt"
+	"strings"
+
+	"advent/lib/util"
+)
+
+func SplitDigits(s string) (output []int) {
+	for _, c := range s {
+		output = append(output, int(c-'0'))
+	}
+	return output
+}
+func JoinDigits(ints []int) string {
+	var b strings.Builder
+	for _, n := range ints {
+		_, _ = fmt.Fprintf(&b, "%d", n)
+	}
+	return b.String()
+}
+func PhaseN(n int, inputs []int) []int {
+	for ; n > 0; n-- {
+		inputs = Phase(inputs)
+	}
+	return inputs
+}
+func Phase(inputs []int) (outputs []int) {
+	repeater := NewRepeater()
+	for range inputs {
+		sum := 0
+		for _, n := range inputs {
+			sum += n * repeater.Next()
+		}
+		repeater.IncrementPeriod()
+		outputs = append(outputs, util.Abs(sum%10))
+	}
+	return outputs
+}
+
+var base = []int{0, 1, 0, -1}
+
+type RepeatingPattern struct {
+	period  int
+	repeats int
+	base    int
+}
+
+func NewRepeater() *RepeatingPattern {
+	return &RepeatingPattern{
+		period:  1,
+		repeats: 1,
+		base:    0,
+	}
+}
+
+func (this *RepeatingPattern) IncrementPeriod() {
+	this.period++
+	this.repeats = 1
+	this.base = 0
+}
+
+func (this *RepeatingPattern) Next() int {
+	if this.repeats%this.period == 0 {
+		this.base++
+	}
+	this.repeats++
+	return base[this.base%4]
+}
+
+func Part1() string {
+	return JoinDigits(PhaseN(100, SplitDigits(util.InputString())))[:8]
 }
 
 func Part2() interface{} {
+
 	return nil
 }
