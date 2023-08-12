608e9afc744cbed9d4033e7386af7e82647c562f
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-08-11 21:39:17 -0600

    Starter template (for use with `gonew`).

---
+++ go/starter/go.mod
@@ -0,0 +1,10 @@
+module github.com/mdwhatcott/advent-of-code/go/starter
+
+go 1.21.0
+
+require (
+	github.com/mdwhatcott/advent-of-code-inputs v1.1.0
+	github.com/mdwhatcott/go-set/v2 v2.0.1
+	github.com/mdwhatcott/must v1.1.0
+	github.com/mdwhatcott/testing v1.2.0
+)
---
+++ go/starter/main_test.go
@@ -0,0 +1,36 @@
+package starter
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/mdwhatcott/advent-of-code-inputs/inputs"
+	_ "github.com/mdwhatcott/go-set/v2/set"
+	_ "github.com/mdwhatcott/must/must"
+	"github.com/mdwhatcott/testing/should"
+)
+
+const TODO = -1
+
+var (
+	inputLines  = inputs.Read(TODO, TODO).Lines()
+	sampleLines = []string{
+		fmt.Sprint(TODO),
+	}
+)
+
+func Test(t *testing.T) {
+	should.So(t, Part1(sampleLines), should.Equal, TODO)
+	should.So(t, Part1(inputLines), should.Equal, TODO)
+
+	should.So(t, Part2(sampleLines), should.Equal, TODO)
+	should.So(t, Part2(inputLines), should.Equal, TODO)
+}
+
+func Part1(lines []string) any {
+	return TODO
+}
+
+func Part2(lines []string) any {
+	return TODO
+}
