fb2f796eed507a35e881e78ae4ae1ba6e01da56f
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-08-11 21:44:30 -0600

    Reset 2022 day 16

--- go/2022/day16/go.mod
+++ go/2022/day16/go.mod
@@ -1,11 +1,10 @@
 module github.com/mdwhatcott/advent-of-code/go/2022/day16
 
-go 1.20
+go 1.21.0
 
 require (
-	github.com/mdwhatcott/advent-of-code-inputs v1.0.1 // indirect
-	github.com/mdwhatcott/funcy v1.7.0 // indirect
-	github.com/mdwhatcott/go-set/v2 v2.0.1 // indirect
-	github.com/mdwhatcott/must v1.1.0 // indirect
-	github.com/mdwhatcott/testing v1.2.0 // indirect
+	github.com/mdwhatcott/advent-of-code-inputs v1.1.0
+	github.com/mdwhatcott/go-set/v2 v2.0.1
+	github.com/mdwhatcott/must v1.1.0
+	github.com/mdwhatcott/testing v1.2.0
 )
--- go/2022/day16/main_test.go
+++ go/2022/day16/main_test.go
@@ -1,85 +1,36 @@
-package main
+package day16
 
 import (
-	"strings"
+	"fmt"
 	"testing"
 
-	"github.com/mdwhatcott/must/strconvmust"
+	"github.com/mdwhatcott/advent-of-code-inputs/inputs"
+	_ "github.com/mdwhatcott/go-set/v2/set"
+	_ "github.com/mdwhatcott/must/must"
+	"github.com/mdwhatcott/testing/should"
+)
+
+const TODO = -1
+
+var (
+	inputLines  = inputs.Read(TODO, TODO).Lines()
+	sampleLines = []string{
+		fmt.Sprint(TODO),
+	}
 )
 
 func Test(t *testing.T) {
-	graph := make(map[string][]string)
-	rates := make(map[string]int)
-	for _, line := range exampleInput { // inputs.Read(2022, 16).Lines() {
-		words := split(line)
-		valve := words[1]
-		flowRate := strconvmust.Atoi(words[5])
-		rates[valve] = flowRate
-		neighbors := words[10:]
-		for _, neighbor := range neighbors {
-			graph[valve] = append(graph[valve], neighbor)
-		}
-	}
+	should.So(t, Part1(sampleLines), should.Equal, TODO)
+	should.So(t, Part1(inputLines), should.Equal, TODO)
 
-	t.Log("rates:", rates)
-	t.Log("graph:", graph)
-
-	cursor := State{
-		Elapsed: 1,
-		At:      "AA",
-		Trail:   "AA",
-		Opened:  "AA",
-	}
-
-	max := 0
-	queue := make([]State, 0)
-	queue = append(queue, cursor)
-	for len(queue) > 0 {
-		current, queue := queue[0], queue[1:]
-		if current.Elapsed >= 2 {
-			if tally := current.Tally(rates); tally > max {
-				max = tally
-				t.Log("setting max:", max)
-			}
-			t.Log("finished branch", current.Trail)
-			continue
-		}
-		if !strings.Contains(current.Opened, current.At) && rates[current.At] > 0 {
-			queue = append(queue, current.Open())
-		}
-		for _, neighbor := range graph[current.At] {
-			queue = append(queue, current.Move(neighbor))
-		}
-	}
-	t.Log("max:", max)
+	should.So(t, Part2(sampleLines), should.Equal, TODO)
+	should.So(t, Part2(inputLines), should.Equal, TODO)
 }
 
-type State struct {
-	Elapsed int    // how many minutes elapsed
-	At      string // current valve
-	Trail   string // concatenated valves
-	Opened  string // concatenation of valves arranged according to the tick they were opened
+func Part1(lines []string) any {
+	return TODO
 }
 
-func (this State) Tally(rates map[string]int) (result int) {
-	for tick, valve := range strings.Split(this.Opened, "|") {
-		result += rates[valve] * (30 - tick)
-	}
-	return result
-}
-func (this State) Move(to string) State {
-	return State{
-		Elapsed: this.Elapsed + 1,
-		At:      to,
-		Trail:   this.Trail + " " + to,
-		Opened:  this.Opened,
-	}
-}
-func (this State) Open() State {
-	return State{
-		Elapsed: this.Elapsed + 1,
-		At:      this.At,
-		Trail:   this.Trail,
-		Opened:  this.Opened + " " + this.At,
-	}
+func Part2(lines []string) any {
+	return TODO
 }
