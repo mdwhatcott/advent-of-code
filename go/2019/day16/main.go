400e1ede1dc1d93cf43aff17c6f7bcbbc6b995da
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-11-06 16:52:17 -0700

    Tinkering w/ 2019 day 16.

--- go/2019/day16/main.go
+++ go/2019/day16/main.go
@@ -8,6 +8,14 @@ import (
 	"github.com/mdwhatcott/advent-of-code-go-lib/util"
 )
 
+func Part1() string {
+	return JoinDigits(PhaseN(100, SplitDigits(util.InputString())))[:8]
+}
+
+func Part2() interface{} {
+	return JoinDigits(PhaseN(100, SplitDigits(strings.Repeat(util.InputString(), 10_000))))[:8]
+}
+
 func SplitDigits(s string) (output []int) {
 	for _, c := range s {
 		output = append(output, int(c-'0'))
@@ -22,38 +30,50 @@ func JoinDigits(ints []int) string {
 	return b.String()
 }
 func PhaseN(n int, inputs []int) []int {
+	repeater := NewRepeater(inputs)
 	for ; n > 0; n-- {
-		inputs = Phase(inputs)
+		fmt.Println("Phase:", n)
+		repeater.Phase()
 	}
-	return inputs
-}
-func Phase(inputs []int) (outputs []int) {
-	repeater := NewRepeater()
-	for range inputs {
-		sum := 0
-		for _, n := range inputs {
-			sum += n * repeater.Next()
-		}
-		repeater.IncrementPeriod()
-		outputs = append(outputs, maths.Abs(sum%10))
-	}
-	return outputs
+	return repeater.inputs
 }
 
 var base = []int{0, 1, 0, -1}
 
 type RepeatingPattern struct {
+	inputs  []int
+	outputs []int
 	period  int
 	repeats int
 	base    int
 }
 
-func NewRepeater() *RepeatingPattern {
-	return &RepeatingPattern{
-		period:  1,
-		repeats: 1,
-		base:    0,
+func NewRepeater(inputs []int) *RepeatingPattern {
+	this := &RepeatingPattern{
+		inputs:  inputs,
+		outputs: make([]int, len(inputs)),
 	}
+	this.Reset()
+	return this
+}
+
+func (this *RepeatingPattern) Reset() {
+	this.period = 1
+	this.repeats = 1
+	this.base = 0
+}
+
+func (this *RepeatingPattern) Phase() {
+	this.Reset()
+	for x := range this.inputs {
+		sum := 0
+		for _, n := range this.inputs {
+			sum += n * this.Next()
+		}
+		this.IncrementPeriod()
+		this.outputs[x] = maths.Abs(sum % 10)
+	}
+	this.inputs, this.outputs = this.outputs, this.inputs
 }
 
 func (this *RepeatingPattern) IncrementPeriod() {
@@ -68,13 +88,4 @@ func (this *RepeatingPattern) Next() int {
 	}
 	this.repeats++
 	return base[this.base%4]
-}
-
-func Part1() string {
-	return JoinDigits(PhaseN(100, SplitDigits(util.InputString())))[:8]
-}
-
-func Part2() interface{} {
-
-	return nil
 }
