360a663d795014e78847c279caab0bc083c0c72b
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-11-23 00:28:15 -0700

    A better direction.

--- go/2018/day15/main.go
+++ go/2018/day15/main.go
@@ -1,1 +1,123 @@
 package starter
+
+import (
+	"container/list"
+	"sort"
+
+	"github.com/mdwhatcott/funcy"
+	"github.com/mdwhatcott/go-set/v2/set"
+	"github.com/mdwhatcott/grid"
+)
+
+type Point grid.Point[int]
+
+func NewPoint(x, y int) Point             { return Point(grid.NewPoint(x, y)) }
+func (this Point) Point() Point           { return this }
+func (this Point) X() int                 { return this.point().X() }
+func (this Point) Y() int                 { return this.point().Y() }
+func (this Point) point() grid.Point[int] { return grid.Point[int](this) }
+
+type (
+	Path []Point
+
+	Pin interface {
+		Point() Point
+	}
+	Mobile interface {
+		GoTo(Point)
+	}
+)
+
+func MapWalls(lines []string) set.Set[Point] {
+	result := set.Of[Point]()
+	for y, line := range lines {
+		for x, char := range line {
+			if char == '#' {
+				result.Add(NewPoint(x, y))
+			}
+		}
+	}
+	return result
+}
+func SortReadingOrder[T Pin](points []T) {
+	sort.SliceStable(points, func(i, j int) bool {
+		I := points[i].Point()
+		J := points[j].Point()
+		if I.Y() == J.Y() {
+			return I.X() < J.X()
+		}
+		return I.Y() < J.Y()
+	})
+}
+func Points[T Pin](units []T) (result []Point) {
+	return funcy.Map(T.Point, units)
+}
+func MoveActor[T Pin](actor T, targets []T, obstacles set.Set[Point]) {
+	// Calculate all paths to all targets:
+	var all []Path
+	for _, target := range targets {
+		all = append(all, findShortestPaths(actor.Point(), target.Point(), obstacles)...)
+	}
+	if len(all) == 0 {
+		return
+	}
+
+	// Determine length of shortest path(s):
+	sort.SliceStable(all, func(i, j int) bool { return len(all[i]) < len(all[j]) })
+	shortestPathLength := len(all[0])
+	if shortestPathLength == 0 {
+		return
+	}
+
+	// Filter the shortest paths and index them by their final step:
+	shortestByLast := make(map[Point][]Path)
+	for _, path := range all {
+		if len(path) > shortestPathLength {
+			break
+		}
+		shortestByLast[path[len(path)-1]] = append(shortestByLast[path[len(path)-1]], path)
+	}
+
+	// Sort (in reading order) the shortest paths by final step:
+	keys := funcy.MapKeys(shortestByLast)
+	SortReadingOrder(keys)
+
+	// Filter the shortest paths and index them by their first step:
+	candidates := shortestByLast[keys[0]]
+	byFirst := make(map[Point]Path)
+	for _, path := range candidates {
+		byFirst[path[0]] = path
+	}
+
+	// Sort (in reading order) the shortest paths to the previously selected last step by their first step:
+	keys = funcy.MapKeys(byFirst)
+	SortReadingOrder(keys)
+
+	// Actor takes first step along best path:
+	best := byFirst[keys[0]]
+	any(actor).(Mobile).GoTo(best[0])
+}
+func findShortestPaths(start, end Point, obstacles set.Set[Point]) (results []Path) {
+	queue := list.New()
+	queue.PushBack([]Point{start})
+	visited := set.Of(start)
+	for queue.Len() > 0 {
+		path := queue.Remove(queue.Front()).([]Point)
+		current := path[len(path)-1].point()
+
+		if grid.CityBlockDistance(current, end.point()) == 1 && len(path) > 1 {
+			results = append(results, path[1:])
+			continue
+		}
+		for _, next := range current.Neighbors4() {
+			point := Point(next)
+			if !obstacles.Contains(point) && !visited.Contains(point) {
+				visited.Add(point)
+				newPath := append([]Point{}, path...)
+				newPath = append(newPath, point)
+				queue.PushBack(newPath)
+			}
+		}
+	}
+	return results
+}
--- go/2018/day15/main_test.go
+++ go/2018/day15/main_test.go
@@ -1,16 +1,20 @@
 package starter
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/mdwhatcott/advent-of-code-inputs/inputs"
+	"github.com/mdwhatcott/go-set/v2/set"
 	"github.com/mdwhatcott/testing/should"
 )
 
 const TODO = -1
 
 func Test(t *testing.T) {
+	t.Skip("not ready yet")
 	inputLines := inputs.Read(2018, 15).Lines()
+	t.Log("\n" + strings.Join(inputLines, "\n"))
 	should.So(t, Part1(inputLines), should.Equal, TODO)
 	should.So(t, Part2(inputLines), should.Equal, TODO)
 }
@@ -18,9 +22,147 @@ func Test(t *testing.T) {
 func Part1(lines []string) any {
 	return TODO
 }
-
 func Part2(lines []string) any {
 	return TODO
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+
+func TestSortReadingOrder(t *testing.T) {
+	points := []*TestUnit{
+		{point: NewPoint(9, 2)},
+		{point: NewPoint(0, 3)},
+		{point: NewPoint(4, 9)},
+		{point: NewPoint(0, 4)},
+		{point: NewPoint(7, 3)},
+		{point: NewPoint(8, 8)},
+		{point: NewPoint(8, 8)},
+		{point: NewPoint(7, 9)},
+		{point: NewPoint(6, 0)},
+		{point: NewPoint(9, 4)},
+	}
+
+	SortReadingOrder(points)
+
+	should.So(t, points, should.Equal, []*TestUnit{
+		{point: NewPoint(6, 0)},
+		{point: NewPoint(9, 2)},
+		{point: NewPoint(0, 3)},
+		{point: NewPoint(7, 3)},
+		{point: NewPoint(0, 4)},
+		{point: NewPoint(9, 4)},
+		{point: NewPoint(8, 8)},
+		{point: NewPoint(8, 8)},
+		{point: NewPoint(4, 9)},
+		{point: NewPoint(7, 9)},
+	})
+}
+
+type TestUnit struct {
+	point Point
+}
+
+func (this *TestUnit) Point() Point {
+	return this.point
+}
+func (this *TestUnit) GoTo(p Point) {
+	this.point = p
+}
+
+func TestMapWalls(t *testing.T) {
+	lines := []string{
+		"#######",
+		"#E..G.#",
+		"#...#.#",
+		"#.G.#G#",
+		"#######",
+	}
+	obstacles := MapWalls(lines)
+	should.So(t, obstacles, should.Equal, set.Of(
+		// top side walls
+		NewPoint(0, 0),
+		NewPoint(1, 0),
+		NewPoint(2, 0),
+		NewPoint(3, 0),
+		NewPoint(4, 0),
+		NewPoint(5, 0),
+		NewPoint(6, 0),
+
+		// left side walls
+		NewPoint(0, 1),
+		NewPoint(0, 2),
+		NewPoint(0, 3),
+
+		// right side walls
+		NewPoint(6, 1),
+		NewPoint(6, 2),
+		NewPoint(6, 3),
+
+		// bottom side walls
+		NewPoint(0, 4),
+		NewPoint(1, 4),
+		NewPoint(2, 4),
+		NewPoint(3, 4),
+		NewPoint(4, 4),
+		NewPoint(5, 4),
+		NewPoint(6, 4),
+
+		// middle walls
+		NewPoint(4, 2),
+		NewPoint(4, 3),
+	))
+}
+func TestNextMove_ExampleA(t *testing.T) {
+	/*
+		Targets:      In range:     Reachable:    Nearest:      Chosen:
+		#######       #######       #######       #######       #######
+		#E..G.#       #E.?G?#       #E.@G.#       #E.!G.#       #E.+G.#
+		#...#.#  -->  #.?.#?#  -->  #.@.#.#  -->  #.!.#.#  -->  #...#.#
+		#.G.#G#       #?G?#G#       #@G@#G#       #!G.#G#       #.G.#G#
+		#######       #######       #######       #######       #######
+	*/
+	walls := MapWalls([]string{
+		"#######",
+		"#E..G.#",
+		"#...#.#",
+		"#.G.#G#",
+		"#######",
+	})
+	goblins := []*TestUnit{
+		{point: NewPoint(4, 1)},
+		{point: NewPoint(3, 2)},
+		{point: NewPoint(5, 3)},
+	}
+	obstacles := walls.Union(set.Of(Points(goblins)...))
+	elf := &TestUnit{point: NewPoint(1, 1)}
+
+	MoveActor(elf, goblins, obstacles)
+
+	should.So(t, elf.Point(), should.Equal, NewPoint(2, 1))
+}
+func TestNextMove_ExampleB(t *testing.T) {
+	/*
+		In range:     Nearest:      Chosen:       Distance:     Step:
+		#######       #######       #######       #######       #######
+		#.E...#       #.E...#       #.E...#       #4E212#       #..E..#
+		#...?.#  -->  #...!.#  -->  #...+.#  -->  #32101#  -->  #.....#
+		#..?G?#       #..!G.#       #...G.#       #432G2#       #...G.#
+		#######       #######       #######       #######       #######
+	*/
+	walls := MapWalls([]string{
+		"#######",
+		"#.E...#",
+		"#.....#",
+		"#...G.#",
+		"#######",
+	})
+	goblins := []*TestUnit{
+		{point: NewPoint(4, 3)},
+	}
+	obstacles := walls.Union(set.Of(Points(goblins)...))
+	elf := &TestUnit{point: NewPoint(2, 1)}
+
+	MoveActor(elf, goblins, obstacles)
+
+	should.So(t, elf.Point(), should.Equal, NewPoint(3, 1))
+}
