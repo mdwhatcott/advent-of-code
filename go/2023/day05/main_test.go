aae2a0e4defa094d50b1651ab58a69ed8f91ed0b
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-12-06 01:37:46 -0700

    Rework 2023 day 5 via reverse-lookup strategy.
    This implementation, which solves both parts is interesting because
    part 1 is solved much more slowly than part 2, which is not how things
    usually work on the advent of code. This is a good example of how
    sometimes making a really fast thing a bit slower can help make a
    really slow thing much faster.
    
    - Part 1 is now much slower (from 0s to 34s)...but
    - Part 2 is now much faster (from 15m to 3s)!
    - That's a net speed improvement of 14m. (I'll take it!)
    
    I had wondered doing a backwards lookup was the way to speed up part 2
    but I couldn't quite visualize it. This implementation was inspired by
    my former mentor's lovely Clojure solution:
    
    https://github.com/slagyr/advent-of-code/blob/master/src/aoc/2023/day5.clj

--- go/2023/day05/main_test.go
+++ go/2023/day05/main_test.go
@@ -1,10 +1,25 @@
+/*
+This implementation, which solves both parts is interesting because
+part 1 is solved much more slowly than part 2, which is not how things
+usually work on the advent of code. This is a good example of how
+sometimes making a really fast thing a bit slower can help make a
+really slow thing much faster.
+
+- Part 1 is now much slower (from 0s to 34s)...but
+- Part 2 is now much faster (from 15m to 3s)!
+- That's a net speed improvement of 14m. (I'll take it!)
+
+I had wondered doing a backwards lookup was the way to speed up part 2
+but I couldn't quite visualize it. This implementation was inspired by
+my former mentor's lovely Clojure solution:
+
+https://github.com/slagyr/advent-of-code/blob/master/src/aoc/2023/day5.clj
+*/
 package day05
 
 import (
-	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/mdwhatcott/advent-of-code-inputs/inputs"
 	"github.com/mdwhatcott/funcy"
@@ -48,107 +63,87 @@ humidity-to-location map:
 60 56 37
 56 93 4
 `, "\n")
+
+	part1sampleSeedRangePairs = rewriteSeedsAsRanges(part2sampleSeedRangePairs)
+	part1actualSeedRangePairs = rewriteSeedsAsRanges(part2actualSeedRangePairs)
+	part2sampleSeedRangePairs = funcy.Map(strconvmust.Atoi, strings.Fields(sampleInput[0][len("seeds:"):]))
+	part2actualSeedRangePairs = funcy.Map(strconvmust.Atoi, strings.Fields(inputLines[0][len("seeds:"):]))
 )
+
+// rewriteSeedsAsRanges turns a line like `seeds: 79 14 55 13` into []int{79, 1, 14, 1, 55, 1, 13, 1}
+// Basically, it makes single seeds look like length-1 ranges, compatible with part 2-style handling.
+func rewriteSeedsAsRanges(numbers []int) (result []int) {
+	for _, number := range numbers {
+		result = append(result, number, 1)
+	}
+	return result
+}
 
 func TestSuite(t *testing.T) {
 	should.Run(&Suite{T: should.New(t)}, should.Options.IntegrationTests())
 }
 
-type Suite struct {
-	*should.T
-	now time.Time
-}
+type Suite struct{ *should.T }
 
-func (this *Suite) Setup() {
-	this.now = time.Now()
-}
 func (this *Suite) TestPart1A() {
-	this.So(this.Solve(sampleInput, this.Part1SeedStreamer), should.Equal, 35)
+	this.So(this.Solve(sampleInput, part1sampleSeedRangePairs), should.Equal, 35)
 }
 func (this *Suite) TestPart1Full() {
-	this.So(this.Solve(inputLines, this.Part1SeedStreamer), should.Equal, 261668924)
+	this.So(this.Solve(inputLines, part1actualSeedRangePairs), should.Equal, 261668924)
 }
 func (this *Suite) TestPart2A() {
-	this.So(this.Solve(sampleInput, this.Part2SeedStreamer), should.Equal, 46)
+	this.So(this.Solve(sampleInput, part2sampleSeedRangePairs), should.Equal, 46)
 }
-func (this *Suite) LongTestPart2Full() {
-	this.So(this.Solve(inputLines, this.Part2SeedStreamer), should.Equal, 24261545)
+func (this *Suite) TestPart2Full() {
+	this.So(this.Solve(inputLines, part2actualSeedRangePairs), should.Equal, 24261545)
 }
-func (this *Suite) Part1SeedStreamer(raw string) chan int {
-	result := make(chan int)
-	go funcy.Load(result, funcy.Map(strconvmust.Atoi, strings.Fields(raw)))
-	return result
-}
-func (this *Suite) Part2SeedStreamer(raw string) chan int {
-	result := make(chan int)
-	numbers := funcy.Map(strconvmust.Atoi, strings.Fields(raw))
-	go func() {
-		sent := 0
-		for x := 0; x < len(numbers); x += 2 {
-			start, count := numbers[x], numbers[x+1]
-			this.Printf("seed range %d/%d starting at %d w/ count %d", x, len(numbers), start, count)
-			for y := start; y < start+count; y++ {
-				result <- y
-				sent++
-			}
-		}
-		close(result)
-		this.Println("seeds:", sent, time.Since(this.now))
-	}()
-	return result
-}
-func (this *Suite) Solve(input []string, seedStreamer func(string) chan int) (result int) {
-	maps := make(map[string]func(int) int)
-	var seeds string
-	title := "seed"
-	var steps []string
-	var ranges []int
-	for _, line := range append(input, "") {
-		if strings.HasPrefix(line, "seeds: ") {
-			seeds = strings.TrimPrefix(line, "seeds: ")
-		} else if line == "" && len(ranges) > 0 {
-			maps[title] = rangeConverter(ranges)
-		} else if strings.HasSuffix(line, " map:") {
-			line = strings.TrimSuffix(line, " map:")
-			words := strings.Split(line, "-")
-			title = words[0]
-			steps = append(steps, title)
-			ranges = nil
-		} else {
-			ranges = append(ranges, funcy.Map(strconvmust.Atoi, strings.Fields(line))...)
+func (this *Suite) Solve(input []string, seedRangePairs []int) (result int) {
+	isSeed := seedChecker(seedRangePairs...)
+	converters := parseConverters(input[2:])
+	for location := 0; ; location++ {
+		candidate := convertAll(location, converters...)
+		if isSeed(candidate) {
+			return location
 		}
 	}
-
-	minLocation := 0xFFFFFFFF
-	progress := 0
-	for seed := range seedStreamer(seeds) {
-		for _, step := range steps {
-			progress++
-			if progress%100_000_000 == 0 {
-				this.Println("progress:", progress, time.Since(this.now))
-			}
-			converter := maps[step]
-			if converter == nil {
-				break
-			}
-			seed = converter(seed)
-		}
-		if seed < minLocation {
-			minLocation = seed
-			this.Println("new min:", seed, time.Since(this.now))
-		}
-	}
-	this.Println("absolute min:", minLocation)
-	return minLocation
 }
-
+func convertAll(location int, converters ...func(int) int) int {
+	for _, converter := range converters {
+		location = converter(location)
+	}
+	return location
+}
+func parseConverters(lines []string) (results []func(int) int) {
+	var numbers []int
+	for x := len(lines) - 1; x >= 0; x-- {
+		line := lines[x]
+		if line == "" {
+			continue
+		}
+		if strings.Contains(line, "map:") {
+			results = append(results, rangeConverter(numbers))
+			numbers = nil
+			continue
+		}
+		numbers = append(numbers, funcy.Map(strconvmust.Atoi, strings.Fields(line))...)
+	}
+	return results
+}
+func seedChecker(rangePairs ...int) func(int) bool {
+	return func(seed int) bool {
+		for x := 0; x < len(rangePairs); x += 2 {
+			lower, count := rangePairs[x], rangePairs[x+1]
+			if lower <= seed && seed <= lower+count {
+				return true
+			}
+		}
+		return false
+	}
+}
 func rangeConverter(numbers []int) func(int) int {
-	if len(numbers)%3 != 0 {
-		panic(fmt.Sprintf("invalid number count: %d", numbers))
-	}
 	return func(i int) int {
 		for x := 0; x < len(numbers); x += 3 {
-			dest, source, length := numbers[x], numbers[x+1], numbers[x+2]
+			source, dest, length := numbers[x], numbers[x+1], numbers[x+2]
 			if source <= i && i < source+length {
 				return dest + (i - source)
 			}
