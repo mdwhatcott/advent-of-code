f6f4f774e5e3f72b669d91e97743e83d718ce108
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2026-08-22 14:01:43 -0600

    Solve 2025 day 9 part 1 in Go

---
+++ go/2025/day09/go.mod
@@ -0,0 +1,5 @@
+module github.com/mdw-aoc/advent-of-code/go/2025/day09
+
+go 1.27
+
+require github.com/mdw-go/testing/v2 v2.0.1
---
+++ go/2025/day09/main_test.go
@@ -0,0 +1,77 @@
+package day09
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/mdw-go/testing/v2/better"
+	"github.com/mdw-go/testing/v2/should"
+	"github.com/mdw-go/testing/v2/suite"
+)
+
+func TestSuite(t *testing.T) {
+	suite.Run(&Suite{T: suite.New(t)}, suite.Options.UnitTests())
+}
+
+type Suite struct {
+	*suite.T
+}
+
+func (this *Suite) TestArea() {
+	this.So(RectangleArea(Point{2, 5}, Point{11, 1}), should.Equal, 50)
+}
+func (this *Suite) TestPart1Sample() {
+	this.So(this.maxArea(this.gatherPoints("sample-input.txt")), should.Equal, 50)
+}
+func (this *Suite) TestPart1() {
+	this.So(this.maxArea(this.gatherPoints("input.txt")), should.Equal, 4729332959)
+}
+func (this *Suite) gatherPoints(filename string) (coords []Point) {
+func (this *Suite) gatherPoints(filename string) (results []Point) {
+	file, err := os.Open(filename)
+	this.So(err, better.BeNil)
+	defer func() { _ = file.Close() }()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		rawX, rawY, _ := strings.Cut(line, ",")
+		var point Point
+		point.X, err = strconv.Atoi(rawX)
+		this.So(err, better.BeNil)
+		point.Y, err = strconv.Atoi(rawY)
+		this.So(err, better.BeNil)
+		results = append(results, point)
+	}
+	this.So(scanner.Err(), better.BeNil)
+	return results
+}
+func (this *Suite) maxArea(coords []Point) (maxArea int) {
+	for p1 := range coords {
+		for p2 := p1 + 1; p2 < len(coords); p2++ {
+			area := RectangleArea(coords[p1], coords[p2])
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
+
+type Point struct {
+	X, Y int
+}
+
+func RectangleArea(p1, p2 Point) int {
+	return (abs(p1.X-p2.X) + 1) * (abs(p1.Y-p2.Y) + 1)
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
